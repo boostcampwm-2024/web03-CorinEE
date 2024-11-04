a15a7bf5e54e9e9f7a910953b826b7813ddc757c
Jun Hee Jung <wjdtka1325@naver.com>
2024-11-04 18:53:42 +0900

    chore: material tailwind 설치 및 적용

--- packages/client/tailwind.config.js
+++ packages/client/tailwind.config.js
@@ -1,9 +1,13 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./index.html','./src/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+const withMT = require('@material-tailwind/react/utils/withMT');
 
+module.exports = withMT({
+	content: [
+		'./index.html',
+		'./src/**/*.{js,ts,jsx,tsx}',
+		'./node_modules/.vite/deps/@material-tailwind_react.js',
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+});
