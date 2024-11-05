c799a676dacdb53fc939af0ddb874c6049711ff2
qwer0114 <skyok203@naver.com>
2024-11-05 14:06:58 +0900

    feat: path 모듈 추가

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,19 +1,29 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
 	resolve: {
 		alias: [
-			{ find: '@', replacement: './src' },
-			{ find: '@components', replacement: './src/components' },
-			{ find: '@api', replacement: './src/api' },
-			{ find: '@constants', replacement: './src/constants' },
-			{ find: '@hooks', replacement: './src/hooks' },
-			{ find: '@types', replacement: './src/types' },
-			{ find: '@utility', replacement: './src/utility' },
-			{ find: '@pages', replacement: './src/pages' },
+			{ find: '@', replacement: path.resolve(__dirname, '/src') },
+			{
+				find: '@components',
+				replacement: path.resolve(__dirname, '/src/components'),
+			},
+			{ find: '@api', replacement: path.resolve(__dirname, '/src/api') },
+			{
+				find: '@constants',
+				replacement: path.resolve(__dirname, './src/constants'),
+			},
+			{ find: '@hooks', replacement: path.resolve(__dirname, '/src/hooks') },
+			{ find: '@types', replacement: path.resolve(__dirname, '/src/types') },
+			{
+				find: '@utility',
+				replacement: path.resolve(__dirname, '/src/utility'),
+			},
+			{ find: '@pages', replacement: path.resolve(__dirname, '/src/pages') },
 		],
 	},
 });
