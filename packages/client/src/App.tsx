5b95d5fbf2b14c3947fb4a15e3d0412dd777735b
qwer0114 <skyok203@naver.com>
2024-11-05 11:11:05 +0900

    feat: Router 정의

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,23 +1,9 @@
-import { Button } from '@material-tailwind/react';
-
-export function ButtonColors() {
-	return (
-		<div className="flex w-max gap-4">
-			<Button color="blue">color blue</Button>
-			<Button color="red">color red</Button>
-			<Button color="green">color green</Button>
-			<Button color="amber">color amber</Button>
-		</div>
-	);
-}
-
-
+import Router from './Router';
 
 function App() {
 	return (
 		<>
-			<div className="text-red-700">App</div>
-			<ButtonColors />
+			<Router />
 		</>
 	);
 }
