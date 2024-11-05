5b34066ae5e9f94436edc7dfa27dbfef69b7eb03
qwer0114 <skyok203@naver.com>
2024-11-05 16:12:38 +0900

    feat: sidebar 정의

--- packages/client/src/pages/layout/Layout.tsx
+++ packages/client/src/pages/layout/Layout.tsx
@@ -1,11 +1,18 @@
+import Header from '@/components/Header';
+import Sidebar from '@/components/Sidebar';
 import { Outlet } from 'react-router-dom';
 
 function Layout() {
 	return (
-		<div>
-			<h1>Layout</h1>
-			<Outlet />
-		</div>
+		<>
+			<div className="flex">
+				<div className="w-screen flex flex-col flex-1">
+					<Header />
+					<Outlet />
+				</div>
+				<Sidebar />
+			</div>
+		</>
 	);
 }
 
