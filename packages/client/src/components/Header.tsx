7095208dc10748a637a5d851e7ffcd5da01b47ec
qwer0114 <skyok203@naver.com>
2024-11-05 18:22:59 +0900

    feat: Header 컴포넌트 디자인

--- packages/client/src/components/Header.tsx
+++ packages/client/src/components/Header.tsx
@@ -1,16 +1,25 @@
-import { Navbar, Typography } from '@material-tailwind/react';
+import { Button, Navbar } from '@material-tailwind/react';
+import { Link } from 'react-router-dom';
 
 function Header() {
 	return (
-		<Navbar fullWidth={true} className="flex justify-end">
-			<Typography
-				as="a"
-				href="#"
-				className="mr-4 cursor-pointer py-1.5 font-medium text-red-300"
-			>
-				Material Tailwind
-			</Typography>
-			<p className="text-red-300">sdf</p>
+		<Navbar fullWidth={true} className="flex justify-between items-center">
+			<div>
+				<Link to={'/'} className="text-black">
+					코린이
+				</Link>
+			</div>
+			<div className="flex gap-4">
+				<Link to={'/'} className="text-gray-600 hover:text-black">
+					홈
+				</Link>
+				<Link to={'/account'} className="text-gray-600 hover:text-black">
+					내 계좌
+				</Link>
+			</div>
+			<div>
+				<Button>로그인</Button>
+			</div>
 		</Navbar>
 	);
 }
