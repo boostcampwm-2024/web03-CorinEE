0217a3adb7d48f8f32ae499fcca184e7ed9a7be0
qwer0114 <skyok203@naver.com>
2024-11-05 11:22:00 +0900

    feat: QueryClientProvider 추가

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,9 +1,21 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Router from './Router';
 
 function App() {
+	const queryClient = new QueryClient({
+		defaultOptions: {
+			queries: {
+				refetchOnWindowFocus: false,
+				refetchOnMount: true,
+				refetchOnReconnect: false,
+			},
+		},
+	});
 	return (
 		<>
-			<Router />
+			<QueryClientProvider client={queryClient}>
+				<Router />
+			</QueryClientProvider>
 		</>
 	);
 }
