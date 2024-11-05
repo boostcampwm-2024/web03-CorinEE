553c5aca75346626887f14e2ddb9ba7189f50176
qwer0114 <skyok203@naver.com>
2024-11-05 16:12:49 +0900

    feat: header 정의

---
+++ packages/client/src/components/Header.tsx
@@ -0,0 +1,18 @@
+import { Navbar, Typography } from '@material-tailwind/react';
+
+function Header() {
+	return (
+		<Navbar className="flex justify-end">
+			<Typography
+				as="a"
+				href="#"
+				className="mr-4 cursor-pointer py-1.5 font-medium text-red-300"
+			>
+				Material Tailwind
+			</Typography>
+			<p className="text-red-300">sdf</p>
+		</Navbar>
+	);
+}
+
+export default Header;
