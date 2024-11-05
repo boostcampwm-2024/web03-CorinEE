17550e1cceb408a4ad9792c792be3620e5cc833f
qwer0114 <skyok203@naver.com>
2024-11-05 13:53:54 +0900

    feat: path alias 설정

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	resolve: {
+		alias: [
+			{ find: '@', replacement: './src' },
+			{ find: '@components', replacement: './src/components' },
+			{ find: '@api', replacement: './src/api' },
+			{ find: '@constants', replacement: './src/constants' },
+			{ find: '@hooks', replacement: './src/hooks' },
+			{ find: '@types', replacement: './src/types' },
+			{ find: '@utility', replacement: './src/utility' },
+			{ find: '@pages', replacement: './src/pages' },
+		],
+	},
+});
