50cb35260d715945d1ee37c4aabe7b2fff21f3eb
qwer0114 <skyok203@naver.com>
2024-11-05 17:51:55 +0900

    feat: Sidebar 디자인 추가

--- packages/client/src/components/Sidebar.tsx
+++ packages/client/src/components/Sidebar.tsx
@@ -1,21 +1,47 @@
 import SideDrawer from '@/components/SideDrawer';
 import { useState } from 'react';
+import Calendar from '@asset/calendar.svg?react';
+import Heart from '@asset/heart.svg?react';
+import Invest from '@asset/invest.svg?react';
+import Fire from '@asset/fire.svg?react';
 function Sidebar() {
 	const [openRight, setOpenRight] = useState(false);
 	const openDrawerRight = () => setOpenRight(!openRight);
 	const closeDrawerRight = () => setOpenRight(false);
 
 	return (
-		<div className="flex bg-gray-100">
+		<div className="flex bg-gray-100 h-screen ">
 			<SideDrawer openRight={openRight} closeDrawerRight={closeDrawerRight} />
-			<div className="w-24 h-screen border-red-100 border">
-				<div>
-					<button
-						onClick={openDrawerRight}
-						className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-					>
-						Open Drawer Right
+			<div className="w-20 flex flex-col gap-3 items-center border-l border-gray-400 border-solid">
+				<div className="flex flex-col items-center gap-2 py-2">
+					<button onClick={openDrawerRight} className="  text-white rounded ">
+						<Invest className="w-6 h-6" />
 					</button>
+					<div className="text-xs text-gray-600">내 투자</div>
+				</div>
+				<div>
+					<div className="flex flex-col items-center gap-2 py-2">
+						<button onClick={openDrawerRight} className="  text-white rounded ">
+							<Heart className="w-6 h-6" />
+						</button>
+						<div className="text-xs text-gray-600">관심</div>
+					</div>
+				</div>
+				<div>
+					<div className="flex flex-col items-center gap-2 py-2">
+						<button onClick={openDrawerRight} className="  text-white rounded ">
+							<Calendar className="w-6 h-6" />
+						</button>
+						<div className="text-xs text-gray-600">최근 본</div>
+					</div>
+				</div>
+				<div>
+					<div className="flex flex-col items-center gap-2 py-2">
+						<button onClick={openDrawerRight} className="  text-white rounded ">
+							<Fire className="w-6 h-6" />
+						</button>
+						<div className="text-xs text-gray-600">실시간</div>
+					</div>
 				</div>
 			</div>
 		</div>
