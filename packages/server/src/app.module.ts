b8c3568fb64dbd6d9cad1003ca9a8f24faa49781
Seonghyeon0409 <t139754@gmail.com>
2024-11-05 23:01:28 +0900

    feat: DB 환경 세팅 - ORM 설정 - ssh 터널링 - user entity

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -1,9 +1,21 @@
+// src/app.module.ts
+
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import getTypeOrmConfig from './configs/typeorm.config';
 
 @Module({
-  imports: [],
+  imports: [
+    AuthModule,
+    TypeOrmModule.forRootAsync({
+      useFactory: async () => {
+        return await getTypeOrmConfig(); // SSH 터널링이 완료된 후 TypeORM 설정 로드
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
