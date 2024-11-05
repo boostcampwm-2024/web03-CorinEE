3e461220bf99ad25e7945c57ec7fb42ab1021254
qwer0114 <skyok203@naver.com>
2024-11-05 18:23:26 +0900

    feat: 사이드바 디자인

--- packages/client/src/components/SideDrawer.tsx
+++ packages/client/src/components/SideDrawer.tsx
@@ -9,14 +9,7 @@ function SideDrawer({ openRight, closeDrawerRight }) {
 			border-l border-gray-400 border-solid
             ${openRight ? 'translate-x-0 w-64' : 'translate-x-full w-0'}
           `}
-			>
-				<button
-					onClick={closeDrawerRight}
-					className="m-4 px-4 py-2 bg-white rounded hover:bg-gray-100"
-				>
-					닫기
-				</button>
-			</div>
+			></div>
 		</div>
 	);
 }
