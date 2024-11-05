1cd70f0082bb1eac732d8fbde46684b733fb2f6b
qwer0114 <skyok203@naver.com>
2024-11-05 18:23:26 +0900

    feat: 사이드바 디자인

--- packages/client/src/components/Sidebar.tsx
+++ packages/client/src/components/Sidebar.tsx
@@ -4,6 +4,7 @@ import Calendar from '@asset/calendar.svg?react';
 import Heart from '@asset/heart.svg?react';
 import Invest from '@asset/invest.svg?react';
 import Fire from '@asset/fire.svg?react';
+
 function Sidebar() {
 	const [openRight, setOpenRight] = useState(false);
 	const openDrawerRight = () => setOpenRight(!openRight);
@@ -13,34 +14,51 @@ function Sidebar() {
 		<div className="flex bg-gray-100 h-screen ">
 			<SideDrawer openRight={openRight} closeDrawerRight={closeDrawerRight} />
 			<div className="w-20 flex flex-col gap-3 items-center border-l border-gray-400 border-solid">
-				<div className="flex flex-col items-center gap-2 py-2">
-					<button onClick={openDrawerRight} className="  text-white rounded ">
-						<Invest className="w-6 h-6" />
+				<div className="group flex flex-col items-center gap-2 py-2">
+					<button
+						onClick={openDrawerRight}
+						className="text-white rounded p-1 group-hover:bg-gray-500"
+					>
+						<Invest className="w-6 h-6 fill-blue-gray-200 group-hover:fill-black" />
 					</button>
-					<div className="text-xs text-gray-600">내 투자</div>
-				</div>
-				<div>
-					<div className="flex flex-col items-center gap-2 py-2">
-						<button onClick={openDrawerRight} className="  text-white rounded ">
-							<Heart className="w-6 h-6" />
-						</button>
-						<div className="text-xs text-gray-600">관심</div>
+					<div className="text-xs text-gray-600 group-hover:text-black">
+						내 투자
 					</div>
 				</div>
-				<div>
-					<div className="flex flex-col items-center gap-2 py-2">
-						<button onClick={openDrawerRight} className="  text-white rounded ">
-							<Calendar className="w-6 h-6" />
-						</button>
-						<div className="text-xs text-gray-600">최근 본</div>
+
+				<div className="group flex flex-col items-center gap-2 py-2">
+					<button
+						onClick={openDrawerRight}
+						className="text-white rounded p-1 group-hover:bg-gray-500"
+					>
+						<Heart className="w-6 h-6 fill-blue-gray-200 group-hover:fill-black" />
+					</button>
+					<div className="text-xs text-gray-600 group-hover:text-black">
+						관심
 					</div>
 				</div>
-				<div>
-					<div className="flex flex-col items-center gap-2 py-2">
-						<button onClick={openDrawerRight} className="  text-white rounded ">
-							<Fire className="w-6 h-6" />
-						</button>
-						<div className="text-xs text-gray-600">실시간</div>
+
+				<div className="group flex flex-col items-center gap-2 py-2">
+					<button
+						onClick={openDrawerRight}
+						className="text-white rounded p-1 group-hover:bg-gray-500"
+					>
+						<Calendar className="w-6 h-6 fill-blue-gray-200 group-hover:fill-black" />
+					</button>
+					<div className="text-xs text-gray-600 group-hover:text-black">
+						최근 본
+					</div>
+				</div>
+
+				<div className="group flex flex-col items-center gap-2 py-2">
+					<button
+						onClick={openDrawerRight}
+						className="text-white rounded p-1 group-hover:bg-gray-500"
+					>
+						<Fire className="w-6 h-6 fill-blue-gray-200 group-hover:fill-black" />
+					</button>
+					<div className="text-xs text-gray-600 group-hover:text-black">
+						실시간
 					</div>
 				</div>
 			</div>
