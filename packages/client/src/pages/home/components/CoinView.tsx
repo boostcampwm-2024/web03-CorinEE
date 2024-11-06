99509be78ee47722b161d338df39467404961afd
Jun Hee Jung <wjdtka1325@naver.com>
2024-11-06 18:35:24 +0900

    faet: 코인 카테고리 컴퍼넌트 분리

--- packages/client/src/pages/home/components/CoinView.tsx
+++ packages/client/src/pages/home/components/CoinView.tsx
@@ -1,36 +1,18 @@
+import CoinCategories from '@/pages/home/components/CoinCategories';
+import { useState } from 'react';
+
 function CoinView() {
+	const [activeCategory, setActiveCategory] = useState('원화');
+
+	const handleCategory = (category) => {
+		setActiveCategory(category);
+	};
+
 	return (
 		<div>
 			<h3 className="text-2xl font-bold text-gray-800">코인 리스트</h3>
 			<div className="text-sm text-gray-700">실시간 코인 가격 확인</div>
-
-			<ul className="flex gap-8 text-lg">
-				<li>
-					<button className="text-gray-800 px-3 bg-gray-100 rounded-md hover:bg-gray-300">
-						원화
-					</button>
-				</li>
-				<li>
-					<button className="text-gray-800 px-3 bg-gray-100 rounded-md hover:bg-gray-300">
-						BTC
-					</button>
-				</li>
-				<li>
-					<button className="text-gray-800 px-3 bg-gray-100 rounded-md hover:bg-gray-300">
-						USDT
-					</button>
-				</li>
-				<li>
-					<button className="text-gray-800 px-3 bg-gray-100 rounded-md hover:bg-gray-300">
-						관심
-					</button>
-				</li>
-				<li>
-					<button className="text-gray-800 px-3 bg-gray-100 rounded-md hover:bg-gray-300">
-						보유
-					</button>
-				</li>
-			</ul>
+			{/* <CoinCategories activeCategory={activeCategory} handleCategory={handleCategory} /> */}
 		</div>
 	);
 }
