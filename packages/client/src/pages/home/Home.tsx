6c04243d39832889793070878a1d55008d1b16b2
Jun Hee Jung <wjdtka1325@naver.com>
2024-11-06 16:36:12 +0900

    feat: 종목 조회 api 연결 확인

--- packages/client/src/pages/home/Home.tsx
+++ packages/client/src/pages/home/Home.tsx
@@ -1,4 +1,12 @@
+import { useMarketAll } from '@/hooks/useMarketAll';
+
 function Home() {
+	const { isPending, error, data } = useMarketAll();
+
+	if (isPending) return 'Loading...';
+
+	if (error) return 'An error has occurred: ' + error.message;
+
 	return <div>Home Page</div>;
 }
 
