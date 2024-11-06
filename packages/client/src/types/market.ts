30b185e99d9505b93a541020669a8393f5ced146
Jun Hee Jung <wjdtka1325@naver.com>
2024-11-06 16:50:37 +0900

    feat: market 데이터 타입 지정

---
+++ packages/client/src/types/market.ts
@@ -0,0 +1,15 @@
+type MarketEvent = {
+	CONCENTRATION_OF_SMALL_ACCOUNTS: boolean;
+	DEPOSIT_AMOUNT_SOARING: boolean;
+	GLOBAL_PRICE_DIFFERENCES: boolean;
+	PRICE_FLUCTUATIONS: boolean;
+	TRADING_VOLUME_SOARING: boolean;
+};
+
+export type MarketData = {
+	market: string;
+	korean_name: string;
+	english_name: string;
+	market_warning?: string;
+	market_event: { warning: boolean; caution: MarketEvent };
+};
