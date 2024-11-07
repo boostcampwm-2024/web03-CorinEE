ffffed5e2a55b7c3010ee9510b28635b4ab657de
qwer0114 <skyok203@naver.com>
2024-11-07 10:53:03 +0900

    refactor: 불필요한 api 호출 제거

--- packages/client/src/pages/home/Home.tsx
+++ packages/client/src/pages/home/Home.tsx
@@ -1,16 +1,6 @@
-import { useMarketAll } from '@/hooks/useMarketAll';
 import CoinView from '@/pages/home/components/CoinView';
-import { filterCoin } from '@/utility/marketDataUtil';
 
 function Home() {
-	const { isPending, error, data } = useMarketAll();
-
-	if (isPending) return 'Loading...';
-
-	if (error) return 'An error has occurred: ' + error.message;
-
-	if (data) console.log(filterCoin(data, 'USDT'));
-
 	return <CoinView />;
 }
 
