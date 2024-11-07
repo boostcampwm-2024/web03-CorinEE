bbcfb214d62404fc741f3c2d5b7ec30537cc7c8a
qwer0114 <skyok203@naver.com>
2024-11-07 16:03:59 +0900

    feat: add server changed file

---
+++ packages/server/src/configs/typeorm.config.ts
@@ -0,0 +1,70 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { config } from 'dotenv';
+import { createTunnel } from 'tunnel-ssh'; 
+
+config();
+
+const env = process.env.NODE_ENV || 'development';
+
+export default async function getTypeOrmConfig(): Promise<TypeOrmModuleOptions> {
+	if(env === 'development') await setupSshTunnel();
+	return {
+		type: process.env.DB_TYPE as 'mysql',
+		host: process.env.DB_HOST,
+		port: Number(process.env.DB_PORT),
+		username: process.env.DB_USERNAME,
+		password: process.env.DB_PASSWORD,
+		database: process.env.DB_DATABASE,
+		entities: [__dirname + '/../**/*.entity.{js,ts}'],
+		synchronize: Boolean(process.env.DB_SYNCHRONIZE), 
+	};
+}
+
+async function setupSshTunnel(): Promise<void> {
+	return new Promise((resolve, reject) => {
+		const sshOptions = {
+			host: process.env.SSH_HOST,
+			port: Number(process.env.SSH_PORT),
+			username: process.env.SSH_USER,
+			password: process.env.SSH_PASSWORD,
+		};
+
+		tunnel(sshOptions, process.env.DB_PORT)
+			.then(() => {
+				resolve();
+			})
+			.catch((error) => {
+				console.error('SSH 터널링 설정 중 오류 발생:', error);
+				reject(error);
+			});
+	});
+}
+
+function tunnel(sshOptions, port, autoClose = true): Promise<void> {
+	return new Promise((resolve, reject) => {
+		const forwardOptions = {
+			srcAddr: process.env.DB_HOST,
+			srcPort: port,
+			dstAddr: process.env.SSH_DB_HOST, // 원격 DB 서버 IP
+			dstPort: Number(process.env.SSH_DB_TUNNEL_PORT), // 원격 DB 포트
+		};
+
+		const tunnelOptions = {
+			autoClose: autoClose,
+		};
+
+		const serverOptions = {
+			port: port,
+		};
+
+		createTunnel(tunnelOptions, serverOptions, sshOptions, forwardOptions)
+			.then((server) => {
+				console.log('SSH 터널링이 성공적으로 설정되었습니다.');
+				resolve();
+			})
+			.catch((error) => {
+				console.error('터널 생성 중 오류 발생:', error);
+				reject(error);
+			});
+	});
+}
