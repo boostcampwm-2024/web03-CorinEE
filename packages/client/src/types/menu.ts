ca0df0581bd23c3ce7169b3e69a42132cbf3674f
qwer0114 <skyok203@naver.com>
2024-11-07 10:53:41 +0900

    refactor: type 지정

--- packages/client/src/types/menu.ts
+++ packages/client/src/types/menu.ts
@@ -1,1 +1,4 @@
+import { Market } from '@/types/market';
+
 export type SideBarMenu = '실시간' | '관심' | '내 투자' | '최근 본' | null;
+export type MarketCategory = Market | 'INTEREST' | 'OWN';
--- packages/client/src/utility/marketDataUtil.ts
+++ packages/client/src/utility/marketDataUtil.ts
@@ -1,9 +1,8 @@
 import { MarketData } from '@/types/market';
-
-type Market = 'KRW' | 'BTC' | 'USDT';
+import { Market } from '@/types/market';
 
 export function filterCoin(data: MarketData[], type: Market) {
-  const regex = new RegExp(`^${type}-\\w+`);
+	const regex = new RegExp(`^${type}-\\w+`);
 
 	return data.filter((data) => regex.test(data.market));
 }
