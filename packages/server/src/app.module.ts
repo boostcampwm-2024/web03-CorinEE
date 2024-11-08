4f2b0a28840aa2fba9141bc835e797f3e746455b
SeungGwa123 <skwan123@naver.com>
2024-11-08 17:47:59 +0900

    feat: healthmodule추가

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -6,10 +6,12 @@ import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import getTypeOrmConfig from './configs/typeorm.config';
+import { HealthModule } from './health/health.module';
 
 @Module({
   imports: [
     AuthModule,
+    HealthModule,
     TypeOrmModule.forRootAsync({
       useFactory: async () => {
         return await getTypeOrmConfig(); // SSH 터널링이 완료된 후 TypeORM 설정 로드
