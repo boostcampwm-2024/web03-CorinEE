25919066e38e41684a89b41e6928b4cc6e3f12db
qwer0114 <skyok203@naver.com>
2024-11-10 21:00:40 +0900

    feat: 비트코인 list item 컴포넌트 분리

--- packages/client/src/pages/home/components/CoinList.tsx
+++ packages/client/src/pages/home/components/CoinList.tsx
@@ -1,8 +1,8 @@
 import { useWSTicker } from '@/hooks/useWSTicker';
+import Coin from '@/pages/home/components/Coin';
 import { MarketData } from '@/types/market';
 import { MarketCategory } from '@/types/menu';
 import { formatData } from '@/utility/formatData';
-import Heart from '@asset/heart.svg?react';
 
 type CoinListProps = {
 	markets: MarketData[];
@@ -12,12 +12,11 @@ type CoinListProps = {
 function CoinList({ markets, activeCategory }: CoinListProps) {
 	const { socketData } = useWSTicker(markets);
 	const formatters = formatData(activeCategory);
-
 	if (!socketData) return;
 
 	return (
 		<div className="w-[90%]">
-			<ul className="flex py-4 border-b border-solid border-gray-300 text-gray-700">
+			<ul className="flex py-4 border-b border-solid border-gray-300 text-gray-700  bg-white">
 				<li className="flex-[1]">관심</li>
 				<li className="flex-[6]">한글명</li>
 				<li className="flex-[6]">현재가</li>
@@ -26,41 +25,12 @@ function CoinList({ markets, activeCategory }: CoinListProps) {
 			</ul>
 
 			{markets.map((market) => (
-				<div
-					className="flex items-center py-1 border-b border-solid border-gray-300"
+				<Coin
 					key={market.market}
-				>
-					<div className="fill-blue-gray-100 flex-[1] flex w-5 h-5">
-						<Heart />
-					</div>
-
-					<div className="flex items-center gap-2 flex-[6]">
-						<img
-							className="w-7 h-7"
-							src={`https://static.upbit.com/logos/${market.market.split('-')[1]}.png`}
-						></img>
-
-						<div className="flex flex-col">
-							<p className="text-gray-700">{market.korean_name}</p>
-							<p className="text-gray-700 text-xs">{market.market}</p>
-						</div>
-					</div>
-
-					<div className="flex-[6]">
-						{formatters.formatTradePrice(
-							socketData[market.market]?.trade_price,
-						)}
-					</div>
-					<div className="flex-[6]">
-						{`${formatters.formatSignedChangePrice(socketData[market.market]?.signed_change_price)}${formatters.formatChangeRate(socketData[market.market]?.signed_change_rate)}
-							`}
-					</div>
-					<div className="flex-[6]">
-						{formatters.formatAccTradePrice(
-							socketData[market.market]?.acc_trade_price_24h,
-						)}
-					</div>
-				</div>
+					formatters={formatters}
+					market={market}
+					socketData={socketData}
+				/>
 			))}
 		</div>
 	);
