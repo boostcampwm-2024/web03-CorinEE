d2ae407c0dbeeeecce44bc7aaac8a4c61dbe3763
qwer0114 <skyok203@naver.com>
2024-11-10 23:22:21 +0900

    feat: query cache 기능 추가

--- packages/client/src/hooks/useMarketAll.ts
+++ packages/client/src/hooks/useMarketAll.ts
@@ -5,6 +5,8 @@ export function useMarketAll() {
 	const { isPending, error, data } = useQuery({
 		queryKey: ['markets'],
 		queryFn: getMarketAll,
+		staleTime: 1000 * 60, // 1분
+		gcTime: 1000 * 60 * 5, // 5분
 	});
 
 	return { isPending, error, data };
