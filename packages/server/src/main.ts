1b6d19122f261d1799457791ec4a7fae85710e1d
Seonghyeon0409 <t139754@gmail.com>
2024-11-11 17:25:25 +0900

    chore: 배포 설정 수정

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,24 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 
-	app.enableCors({
-		origin: ['http://localhost:5173', 'http://175.106.98.147'], // 허용할 클라이언트 주소
-		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-		credentials: true,
-	});
-	app.useGlobalPipes(
-		new ValidationPipe({
-			whitelist: true,
-			transform: true,
-			transformOptions: {
-				enableImplicitConversion: true,
-			},
-		}),
-	);
+	// app.enableCors({
+	// 	origin: ['http://localhost:5173', 'http://175.106.98.147'], // 허용할 클라이언트 주소
+	// 	methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+	// 	credentials: true,
+	// });
 
 	await app.listen(process.env.PORT ?? 3000);
 }
