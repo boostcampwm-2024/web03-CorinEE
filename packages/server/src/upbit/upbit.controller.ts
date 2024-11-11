20ca0c502847abe3bc61274ff54285153254c834
Seonghyeon0409 <t139754@gmail.com>
2024-11-11 12:06:26 +0900

    feat: 코인 정보 upbit api로 받아오기
    Co-authored-by: 이승관 <SeungGwan123@users.noreply.github.com>

--- packages/server/src/upbit/upbit.controller.ts
+++ packages/server/src/upbit/upbit.controller.ts
@@ -1,15 +1,20 @@
 import { Controller, Sse } from '@nestjs/common';
 import { UpbitService } from './upbit.service';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { Observable, Subject } from 'rxjs';
+import { map, takeUntil } from 'rxjs/operators';
 
 @Controller('upbit')
 export class UpbitController {
+
+  private destroy$ = new Subject<void>();
+
   constructor(private readonly upbitService: UpbitService) {}
 
   @Sse('price-updates')
   priceUpdates(): Observable<MessageEvent> {
-    return this.upbitService.getPriceUpdatesStream().pipe(
+    return this.upbitService.getPriceUpdatesStream()
+    .pipe(
+      takeUntil(this.destroy$),
       map((data) => {
         // MessageEvent 타입에 맞게 필요한 속성 추가
         return new MessageEvent('price-update', {
