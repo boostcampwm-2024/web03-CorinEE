08a91fcfaaac74b040ba2b66cc65da117c1a432b
Seonghyeon0409 <t139754@gmail.com>
2024-11-11 12:06:26 +0900

    feat: 코인 정보 upbit api로 받아오기
    Co-authored-by: 이승관 <SeungGwan123@users.noreply.github.com>

--- packages/server/src/upbit/upbit.service.ts
+++ packages/server/src/upbit/upbit.service.ts
@@ -1,34 +1,34 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import * as WebSocket from 'ws';
-import { SseService } from './sse.service';
-import { CoinListService } from './coin-list.service';
-import { UPBIT_WEBSOCKET_CONNECTION_TIME, UPBIT_WEBSOCKET_URL } from 'common/upbit';
-import { CoinTickerDto } from './dtos/coin-ticker.dto';
 
 @Injectable()
-export class UpbitService {
-	private websocket: WebSocket;
+export class UpbitService implements OnModuleInit {
+	private ws: WebSocket;
+  private priceUpdates$: Subject<any> = new Subject(); // SSE로 전달할 스트림 생성
 
-	constructor(
-		private readonly coinListService: CoinListService,
-		private readonly sseService: SseService
-	) {};
+	onModuleInit() {
+		this.connectWebSocket();
+	}
 
-	connectWebSocket(coins: string[]) {
-		this.websocket = new WebSocket(UPBIT_WEBSOCKET_URL);
-		
-		this.websocket.on('open', async () => {
-			await this.coinListService.getCoinListFromUpbit();
-			const coin_list = this.coinListService.getCoinList(coins);
+	private connectWebSocket() {
+		this.ws = new WebSocket('wss://api.upbit.com/websocket/v1');
+
+		this.ws.on('open', () => {
 			console.log('WebSocket 연결 성공');
 			const subscribeMessage = JSON.stringify([
 				{ ticket: 'test' },
-				{ type: 'ticker', codes: coin_list }, 
+				{ type: 'ticker', codes: ['KRW-BTC', 'KRW-ETH'] }, // 원하는 코인 추가
 			]);
 			this.websocket.send(subscribeMessage);
 		});
 
-		this.websocket.on('close', () => {
+		this.ws.on('message', (data) => {
+      //TODO: upbitTicker 타입으로 변경
+			const message = JSON.parse(data.toString());
+      this.priceUpdates$.next(message); // SSE로 전달하기 위해 데이터 방출
+		});
+
+		this.ws.on('close', () => {
 			console.log('WebSocket 연결이 닫혔습니다. 재연결 시도 중...');
 			setTimeout(() => this.connectWebSocket(coins), UPBIT_WEBSOCKET_CONNECTION_TIME);
 		});
