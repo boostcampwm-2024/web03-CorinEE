32696bf0b41f6f8d2a76b3ae43639e830bf84463
qwer0114 <skyok203@naver.com>
2024-11-11 22:20:28 +0900

    feat: 코인 상세페이지 전체적인 레이아웃 정의

--- packages/client/src/pages/trade/Trade.tsx
+++ packages/client/src/pages/trade/Trade.tsx
@@ -1,5 +1,26 @@
+import Chart from '@/pages/trade/components/Chart';
+import OrderBook from '@/pages/trade/components/OrderBook';
+import OrderForm from '@/pages/trade/components/order_form/OrderForm';
+import TradeHeader from '@/pages/trade/components/trade_header/TradeHeader';
+import { useParams } from 'react-router-dom';
+import { useWSTicker } from '@/hooks/useWSTicker';
+
 function Trade() {
-	return <div>Trade Page</div>;
+	const { market } = useParams();
+	const { socketData } = useWSTicker([{ market: market }]);
+	if (!market) return;
+	if (!socketData) return;
+	const currentPrice = socketData[market]?.trade_price;
+	return (
+		<div className="w-full h-full gap-2">
+			<TradeHeader market={market} socketData={socketData} />
+			<div className="flex gap-2 min-h-[700px]">
+				<Chart />
+				<OrderBook />
+				<OrderForm currentPrice={currentPrice} />
+			</div>
+		</div>
+	);
 }
 
 export default Trade;
