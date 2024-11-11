136a9e98681dea22a14cc8c613d4499e334f2f15
Jun Hee Jung <wjdtka1325@naver.com>
2024-11-11 17:47:05 +0900

    feat: 현재 보고 있는 스크롤 페이지 버튼 활성화

--- packages/client/src/pages/home/components/ScrollPageButton.tsx
+++ packages/client/src/pages/home/components/ScrollPageButton.tsx
@@ -1,19 +1,26 @@
 type ScrollPageButtonProps = {
 	pageNumber: number;
+	currentScrollPage: number;
 	onChangeScrollPage: (pageNumber: number) => void;
 };
 
 function ScrollPageButton({
 	pageNumber,
 	onChangeScrollPage,
+	currentScrollPage,
 }: ScrollPageButtonProps) {
+	const isActive = pageNumber === currentScrollPage;
+
 	return (
 		<ol
 			onClick={() => {
 				onChangeScrollPage(pageNumber);
 			}}
 		>
-			<button>{pageNumber}</button>
+			<button className={`w-7 hover:bg-blue-gray-100 rounded-full 
+      ${isActive ? 'text-black bg-blue-gray-100' :'text-gray-600'}`}>
+				{pageNumber}
+			</button>
 		</ol>
 	);
 }
