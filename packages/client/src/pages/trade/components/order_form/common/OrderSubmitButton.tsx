56d281891e4113b5285674e1745fe05ba947fd58
qwer0114 <skyok203@naver.com>
2024-11-11 22:22:11 +0900

    feat: 매수/매도 submit button 공통 컴포넌트로 분리

---
+++ packages/client/src/pages/trade/components/order_form/common/OrderSubmitButton.tsx
@@ -0,0 +1,23 @@
+type OrderSubmitButtonProps = {
+	type: 'buy' | 'sell';
+	onClick?: () => void;
+};
+function OrderSubmitButton({ type, onClick }: OrderSubmitButtonProps) {
+	const buttonStyle =
+		type === 'buy'
+			? 'bg-red-400 hover:bg-red-500'
+			: 'bg-blue-400 hover:bg-blue-500';
+	const text = type === 'buy' ? '구매하기' : '판매하기';
+
+	return (
+		<button
+			className={`w-full h-10 rounded-md text-white mt-10 ${buttonStyle}`}
+			onClick={onClick}
+			type="submit"
+		>
+			{text}
+		</button>
+	);
+}
+
+export default OrderSubmitButton;
