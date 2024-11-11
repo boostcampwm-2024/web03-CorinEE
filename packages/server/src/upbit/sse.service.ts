6e482d7919ed740e6ab8f5af7893cee7c7d713f3
Seonghyeon0409 <t139754@gmail.com>
2024-11-11 14:34:26 +0900

    feat: coinTickerDTO
    Co-authored-by: 이승관 <SeungGwan123@users.noreply.github.com>

--- packages/server/src/upbit/sse.service.ts
+++ packages/server/src/upbit/sse.service.ts
@@ -1,18 +1,24 @@
 import { Injectable } from '@nestjs/common';
 import { Subject, Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, takeUntil } from 'rxjs/operators';
 
 @Injectable()
 export class SseService {
-  private eventStream$ = new Subject<any>();
+	private eventStream$ = new Subject<any>();
+	private destroy$ = new Subject<void>();
 
-  // 데이터 스트림 방출
-  sendEvent(data: any) {
-    this.eventStream$.next(data);
-  }
+	sendEvent(data: any) {
+		this.eventStream$.next(data);
+	}
 
-  // SSE 컨트롤러에서 구독할 스트림 반환
-  getPriceUpdatesStream(): Observable<MessageEvent> {
-    return this.eventStream$.asObservable();
-  }
+	getPriceUpdatesStream(): Observable<MessageEvent> {
+		return this.eventStream$.asObservable().pipe(
+			takeUntil(this.destroy$),
+			map((data) => {
+				return new MessageEvent('price-update', {
+					data: JSON.stringify(data),
+				}) as MessageEvent;
+			}),
+		);
+	}
 }
