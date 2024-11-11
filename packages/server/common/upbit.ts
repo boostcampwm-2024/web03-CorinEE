78d154c8dddb8c4bdbe4e43882e8ff31fe82f2aa
Seonghyeon0409 <t139754@gmail.com>
2024-11-11 14:34:26 +0900

    feat: coinTickerDTO
    Co-authored-by: 이승관 <SeungGwan123@users.noreply.github.com>

---
+++ packages/server/common/upbit.ts
@@ -0,0 +1,4 @@
+export const UPBIT_WEBSOCKET_URL = 'wss://api.upbit.com/websocket/v1';
+export const UPBIT_RESTAPI_URL = 'https://api.upbit.com/v1/market/all?is_details=true'
+export const UPBIT_WEBSOCKET_CONNECTION_TIME = 3000
+export const UPBIT_IMAGE_URL = "https://static.upbit.com/logos/"
--- packages/server/src/upbit/upbit.controller.ts
+++ packages/server/src/upbit/upbit.controller.ts
@@ -1,25 +1,14 @@
 import { Controller, Sse } from '@nestjs/common';
 import { Observable, Subject } from 'rxjs';
-import { map, takeUntil } from 'rxjs/operators';
 import { SseService } from './sse.service';
 
 @Controller('upbit')
 export class UpbitController {
-
-  private destroy$ = new Subject<void>();
 
   constructor(private readonly sseService: SseService) {}
 
   @Sse('price-updates')
   priceUpdates(): Observable<MessageEvent> {
     return this.sseService.getPriceUpdatesStream()
-    .pipe(
-      takeUntil(this.destroy$),
-      map((data) => {
-        return new MessageEvent('price-update', {
-          data: JSON.stringify(data),
-        }) as MessageEvent;
-      }),
-    );
   }
 }
