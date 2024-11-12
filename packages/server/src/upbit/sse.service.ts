254fad74d26fa78c59453bdb042e014c19d50de8
Seonghyeon0409 <t139754@gmail.com>
2024-11-12 17:27:02 +0900

    feat: 코인 종목 api, 호가창 websocket 연결

--- packages/server/src/upbit/sse.service.ts
+++ packages/server/src/upbit/sse.service.ts
@@ -4,16 +4,22 @@ import { map, takeUntil, filter } from 'rxjs/operators';
 
 @Injectable()
 export class SseService implements OnModuleDestroy{
-	private eventStream$ = new Subject<any>();
-	private destroy$ = new Subject<void>();
-
-	sendEvent(data: any) {
-		this.eventStream$.next(data);
+	private coinTickerStream$ = new Subject<any>();
+	private orderbookStream$ = new Subject<any>();
+	private coinTickerdestroy$ = new Subject<void>();
+	private orderbookdestroy$ = new Subject<void>();
+	private coinTicker = 0;
+	coinTickerData(data: any) {
+		this.coinTicker++;
+		this.coinTickerStream$.next(data);
+	}
+	orderbookData(data:any){
+		this.orderbookStream$.next(data);
 	}
 
 	getPriceUpdatesStream(coins, dto:Function): Observable<MessageEvent> {
-		return this.eventStream$.asObservable().pipe(
-			takeUntil(this.destroy$),
+		return this.coinTickerStream$.asObservable().pipe(
+			takeUntil(this.coinTickerdestroy$),
 			filter((data)=>coins.includes(data.code)),
 			map((data) => {
 				//const setDto = dto(data);
@@ -25,7 +31,21 @@ export class SseService implements OnModuleDestroy{
 		);
 	}
 	
+	getOrderbookUpdatesStream(coins, dto:Function): Observable<MessageEvent> {
+		return this.orderbookStream$.asObservable().pipe(
+			takeUntil(this.orderbookdestroy$),
+			filter((data)=> coins.includes(data.code)),
+			map((data) => {
+				//const setDto = dto(data);
+				return new MessageEvent('orderbook-update', {
+					//data: JSON.stringify(setDto),
+					data: JSON.stringify(data),
+				}) as MessageEvent;
+			}),
+		);
+	}
 	onModuleDestroy() {
-		this.destroy$.next();
+		this.coinTickerdestroy$.next();
+		this.orderbookdestroy$.next();
 	}
 }
