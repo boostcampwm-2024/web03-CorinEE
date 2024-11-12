46fedce844d85128486a954891d01fb3831194b2
qwer0114 <skyok203@naver.com>
2024-11-12 11:15:06 +0900

    hotfix: NotLogin 컴포넌트 size props 전달

--- packages/client/src/pages/trade/components/order_form/OrderWaitForm.tsx
+++ packages/client/src/pages/trade/components/order_form/OrderWaitForm.tsx
@@ -1,9 +1,9 @@
 import { useState } from 'react';
-import NotLogin from '@/pages/trade/components/order_form/NotLogin';
+import NotLogin from '@/components/NotLogin';
 
 function OrderWaitForm() {
 	const [isLogin] = useState(false);
-	if (!isLogin) return <NotLogin />;
+	if (!isLogin) return <NotLogin size="md" />;
 	return <div>order wait</div>;
 }
 
