af1713fa497b5ed415e06f4d7a5e2c30edcc800d
qwer0114 <skyok203@naver.com>
2024-11-12 16:52:08 +0900

    feat: SSE 커스텀 훅 사용

--- packages/client/src/pages/trade/Trade.tsx
+++ packages/client/src/pages/trade/Trade.tsx
@@ -3,17 +3,18 @@ import OrderBook from '@/pages/trade/components/OrderBook';
 import OrderForm from '@/pages/trade/components/order_form/OrderForm';
 import TradeHeader from '@/pages/trade/components/trade_header/TradeHeader';
 import { useParams } from 'react-router-dom';
-import { useWSTicker } from '@/hooks/useWSTicker';
-
+import { useSSETicker } from '@/hooks/useSSETicker';
+import { useMemo } from 'react';
 function Trade() {
 	const { market } = useParams();
-	const { socketData } = useWSTicker([{ market: market! }]);
+	const marketCode = useMemo(() => (market ? [{ market }] : []), [market]);
+	const { sseData } = useSSETicker(marketCode);
 	if (!market) return;
-	if (!socketData) return;
-	const currentPrice = socketData[market]?.trade_price;
+	if (!sseData) return;
+	const currentPrice = sseData[market]?.trade_price;
 	return (
 		<div className="w-full h-full gap-2">
-			<TradeHeader market={market} socketData={socketData} />
+			<TradeHeader market={market} sseData={sseData} />
 			<div className="flex gap-2 min-h-[700px]">
 				<Chart />
 				<OrderBook />
