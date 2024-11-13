711497bf5b1179d12a5c80b1209233acc3f7cb47
qwer0114 <skyok203@naver.com>
2024-11-13 11:05:08 +0900

    feat: Chart 컴포넌트 데이터 패칭 및 컴포넌트 분리

--- packages/client/src/pages/trade/components/Chart.tsx
+++
@@ -1,5 +0,0 @@
-function Chart() {
-	return <div className="bg-gray-50 w-2/3 min-w-96 rounded-lg">Chart</div>;
-}
-
-export default Chart;
---
+++ packages/client/src/pages/trade/components/chart/Chart.tsx
@@ -0,0 +1,25 @@
+import { usePeriodChart } from '@/hooks/usePeriodChart';
+import { useState } from 'react';
+import ChartSelector from '@/pages/trade/components/chart/ChartSelector';
+import { CandlePeriod } from '@/types/chart';
+import CandleChart from '@/pages/trade/components/chart/CandleChart';
+function Chart({ market }: { market: string }) {
+	const [activePeriod, setActivePeriod] = useState<CandlePeriod>('days');
+
+	const { data } = usePeriodChart(market, activePeriod);
+	const handleActivePeriod = (period: CandlePeriod) => {
+		setActivePeriod(period);
+	};
+
+	return (
+		<div className="bg-gray-50 rounded-lg flex-[2] overflow-hidden">
+			<ChartSelector
+				activePeriods={activePeriod}
+				handleActivePeriod={handleActivePeriod}
+			/>
+			<CandleChart data={data} />
+		</div>
+	);
+}
+
+export default Chart;
