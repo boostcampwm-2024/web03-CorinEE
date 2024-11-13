3870f620cd92ebf5b1d34f819707cc1352ad7ccb
qwer0114 <skyok203@naver.com>
2024-11-13 18:37:55 +0900

    feat: 매수/매도 가격 정보 컴포넌트 구현

---
+++ packages/client/src/pages/trade/components/order_book/OrderBookPrice.tsx
@@ -0,0 +1,20 @@
+type OrderBookPriceProps = {
+	currentPrice: number;
+	price: number;
+	rate: string;
+};
+
+function OrderBookPrice({ currentPrice, price, rate }: OrderBookPriceProps) {
+	const textColor = rate.includes('-') ? 'text-blue-600' : 'text-red-500';
+	const borderStyle =
+		currentPrice === price ? 'border border-black border-solid' : '';
+
+	return (
+		<div className={`col-span-2 text-center ${textColor} ${borderStyle}`}>
+			<span className="text-center flex-1 p-3">{price.toLocaleString()}</span>
+			<span className="text-xs">{rate}</span>
+		</div>
+	);
+}
+
+export default OrderBookPrice;
