a40eab0f2efa01d12022d65964f829484177a3d7
Jun Hee Jung <wjdtka1325@naver.com>
2024-11-13 16:13:11 +0900

    feat: 사이드바 실시간 탭 내의 각 코인별 컴포넌트 구현

---
+++ packages/client/src/components/sidebar/SidebarCoin.tsx
@@ -0,0 +1,50 @@
+import colorClasses from '@/constants/priceColor';
+import { Change, SSEDataType } from '@/types/ticker';
+import { Formatters } from '@/utility/formatData';
+
+type SidebarCoinProps = {
+	image_url: string;
+	korean_name: string;
+	listNumber: number;
+	formatters: Formatters;
+	sseData: SSEDataType;
+	market: string;
+};
+
+function SidebarCoin({
+	listNumber,
+	image_url,
+	korean_name,
+	formatters,
+	sseData,
+	market,
+}: SidebarCoinProps) {
+	const change: Change = sseData[market]?.change;
+
+	const trade_price = formatters.formatTradePrice(sseData[market]?.trade_price);
+	const change_rate = formatters.formatChangeRate(
+		sseData[market]?.signed_change_rate,
+		change,
+	);
+	const change_price = formatters.formatSignedChangePrice(
+		sseData[market]?.signed_change_price,
+		change,
+	);
+	return (
+		<div className="flex items-center py-2 px-1 gap-2">
+			<span className="text-base font-medium text-blue-700">{listNumber}</span>
+			<img className="w-8 -h-8" src={image_url}></img>
+			<span className="text-sm flex-[1] text-gray-800 font-semibold">
+				{korean_name}
+			</span>
+			<div className="flex flex-col items-end">
+				<span className="text-base font-semibold">{`${trade_price}`}</span>
+				<span className={`text-xs font-medium ${colorClasses[change]}`}>
+					{change_price}({change_rate})
+				</span>
+			</div>
+		</div>
+	);
+}
+
+export default SidebarCoin;
