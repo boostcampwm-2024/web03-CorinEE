f676b33f8b400e86e78e0e072b1c7893765d2fcf
Jun Hee Jung <wjdtka1325@naver.com>
2024-11-13 16:12:44 +0900

    feat: 사이드바 실시간 탭 구현

--- packages/client/src/components/sidebar/Realtime.tsx
+++ packages/client/src/components/sidebar/Realtime.tsx
@@ -1,5 +1,37 @@
+import SidebarCoin from '@/components/sidebar/SidebarCoin';
+import { useCurrentTime } from '@/hooks/useCurrentTIme';
+import { useMarketTop20 } from '@/hooks/useMarketTop20';
+import { useSSETicker } from '@/hooks/useSSETicker';
+import { formatData } from '@/utility/formatData';
+
 function Realtime() {
-	return <div>Realtime</div>;
+	const { data: top20List } = useMarketTop20();
+	const currentTime = useCurrentTime();
+	const { sseData } = useSSETicker(top20List);
+	if(!sseData || !top20List) return 
+	const formatters = formatData('KRW');
+
+
+	return (
+		<div className="flex flex-col h-full overflow-y-auto [&::-webkit-scrollbar]:hidden p-4">
+			<div>
+				<span className="text-lg font-semibold">실시간 차트</span>
+				<span className="ml-2 text-xs text-gray-700">
+					오늘 {currentTime} 기준
+				</span>
+			</div>
+			<div className="border border-solid border-gray-300 my-3"></div>
+			{top20List?.map((coin, index) => (
+				<SidebarCoin
+					key={coin.market}
+					listNumber={index + 1}
+					formatters= {formatters}
+					sseData={sseData}
+					{...coin}
+				/>
+			))}
+		</div>
+	);
 }
 
 export default Realtime;
