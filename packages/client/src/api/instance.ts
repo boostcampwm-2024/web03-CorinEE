e260f528724d314fc6d3ad6b3136d99a7c6165a3
Jun Hee Jung <wjdtka1325@naver.com>
2024-11-13 18:20:26 +0900

    fix: 차트는 아직 업비트 API를 사용 중이므로 업비트 인스턴스 추가

--- packages/client/src/api/instance.ts
+++ packages/client/src/api/instance.ts
@@ -4,3 +4,8 @@ export const instance = axios.create({
 	baseURL: ' http://175.106.98.147:3000',
 	timeout: 2000,
 });
+
+export const upbitInstance = axios.create({
+	baseURL: ' https://api.upbit.com/v1',
+	timeout: 2000,
+});
