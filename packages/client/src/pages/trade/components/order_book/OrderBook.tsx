3005fe51735a0b2d8ada6f5cd97fac6d9f320269
qwer0114 <skyok203@naver.com>
2024-11-13 18:37:16 +0900

    feat: 호가창 컴포넌트 정의

---
+++ packages/client/src/pages/trade/components/order_book/OrderBook.tsx
@@ -0,0 +1,28 @@
+import AskList from '@/pages/trade/components/order_book/AskList';
+import BidList from '@/pages/trade/components/order_book/BidList';
+import { OrderBook as OrderBookType } from '@/types/orderbook';
+import { formatAsks, formatBids } from '@/utility/formatOrderBookData';
+
+type OrderBookProps = {
+	orderBook: OrderBookType;
+	currentPrice: number;
+};
+
+function OrderBook({ orderBook, currentPrice }: OrderBookProps) {
+	const asks = formatAsks(orderBook);
+	const bids = formatBids(orderBook);
+
+	return (
+		<div className="bg-gray-50 flex-1 min-w-80 rounded-lg p-2 overflow-y-scroll overflow-x-hidden">
+			<div className="text-sm font-semibold border-b border-solid border-gray-300 pb-2">
+				호가
+			</div>
+			<div className="w-full flex flex-col">
+				<AskList asks={asks} currentPrice={currentPrice} />
+				<BidList bids={bids} currentPrice={currentPrice} />
+			</div>
+		</div>
+	);
+}
+
+export default OrderBook;
