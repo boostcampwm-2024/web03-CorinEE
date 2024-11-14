2210cea45d086f3208c27faebf1a03862d286bf9
SeungGwa123 <skwan123@naver.com>
2024-11-14 14:25:13 +0900

    chore: swagger 인증 적용

--- packages/server/src/trade/trade.controller.ts
+++ packages/server/src/trade/trade.controller.ts
@@ -1,12 +1,15 @@
 import { Body, Controller, Get, Query, Param, Request, UseGuards } from '@nestjs/common';
 import { TradeService } from './trade.service';
 import { AuthGuard } from 'src/auth/auth.guard';
+import { ApiBearerAuth, ApiSecurity } from '@nestjs/swagger';
 
 @Controller('trade')
 export class TradeController {
 	constructor(private tradeService: TradeService) {}
 
-  @UseGuards(AuthGuard)
+	@ApiBearerAuth('access-token') 
+	@ApiSecurity('access-token')
+  	@UseGuards(AuthGuard)
 	@Get('calculate-percentage-buy/:moneyType')
 	calculatePercentBuy(
         @Request() req,
