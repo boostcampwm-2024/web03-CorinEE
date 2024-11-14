fcfdd1283d5bb0b79cd0771bc65d7aa480b50dd9
Jun Hee Jung <wjdtka1325@naver.com>
2024-11-14 14:16:06 +0900

    fix: 사이드바 새로고침 했을 때 이전 탭 반영 안되던 버그 수정

--- packages/client/src/utility/typeGuard.ts
+++ packages/client/src/utility/typeGuard.ts
@@ -3,10 +3,10 @@ import { MarketCategory, SideBarMenu } from '@/types/menu';
 
 export function isSideBarMenu(value: string | null): value is SideBarMenu {
 	return (
-		value === '실시간' ||
-		value === '관심' ||
-		value === '내 투자' ||
-		value === '최근 본' ||
+		value === 'REALTIME' ||
+		value === 'INTEREST' ||
+		value === 'MY_INVESTMENT' ||
+		value === 'RECENTLY_VIEWED' ||
 		value === null
 	);
 }
