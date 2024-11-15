0855c1eaa9cebdb535a98c46dc4bef4a9faf4262
Jun Hee Jung <wjdtka1325@naver.com>
2024-11-15 10:23:55 +0900

    feat: 사이드 바 최근 본 카테고리 컴포넌트 구현

--- packages/client/src/components/sidebar/RecentlyViewed.tsx
+++ packages/client/src/components/sidebar/RecentlyViewed.tsx
@@ -1,7 +1,43 @@
-import NotLogin from "@/components/NotLogin";
+import SidebarCoin from '@/components/sidebar/SidebarCoin';
+import { useSSETicker } from '@/hooks/useSSETicker';
+import { formatData } from '@/utility/formatData';
+import useRecentlyMarketStore from '@/store/recentlyViewed';
+import { convertToQueryString } from '@/utility/marketDataUtil';
+import { useRecentlyMarket } from '@/hooks/useRecentlyMarket';
 
 function RecentlyViewed() {
-	return <div><NotLogin size="sm"/></div>;
+	const { recentlyViewedMarketList } = useRecentlyMarketStore();
+	const { data: viewedMarket } = useRecentlyMarket(
+		convertToQueryString(recentlyViewedMarketList),
+	);
+	const { sseData } = useSSETicker(viewedMarket || []);
+
+	if (!sseData || !viewedMarket) return;
+
+	const formatters = formatData('KRW');
+
+	return (
+		<div className="flex flex-col h-full overflow-y-auto [&::-webkit-scrollbar]:hidden p-4">
+			<div>
+				<span className="text-lg font-semibold">최근 본 종목</span>
+				<span className="ml-2 text-xs text-gray-700">
+					10개 종목만 표시
+				</span>
+			</div>
+			<div className="border border-solid border-gray-300 my-3"></div>
+			{recentlyViewedMarketList.length !== 0
+				? viewedMarket.map((coin, index) => (
+						<SidebarCoin
+							key={coin.market}
+							listNumber={index + 1}
+							formatters={formatters}
+							sseData={sseData}
+							{...coin}
+						/>
+					))
+				: '최근 본 항목이 없어요'}
+		</div>
+	);
 }
 
 export default RecentlyViewed;
