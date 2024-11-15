f40141d07dc046efd481d689adcb9f1b3282da15
Jun Hee Jung <wjdtka1325@naver.com>
2024-11-15 10:21:53 +0900

    feat: 최근 본 마켓 종목에 대한 정보를 가져오는 커스텀 훅 구현

---
+++ packages/client/src/hooks/useRecentlyMarket.ts
@@ -0,0 +1,13 @@
+import { useQuery } from '@tanstack/react-query';
+import { getRecentlyMarketList } from '@/api/coin';
+
+export function useRecentlyMarket(queryString: string) {
+	const { isPending, error, data } = useQuery({
+		queryKey: ['recentlyMarketList'],
+		queryFn: () => getRecentlyMarketList(queryString),
+		staleTime: 1000 * 60, // 1분
+		gcTime: 1000 * 60 * 5, // 5분
+	});
+
+	return { isPending, error, data };
+}
