21cce6dafd6302b11b9478d794e1c5b190b3ae95
SeungGwa123 <skwan123@naver.com>
2024-11-15 19:12:21 +0900

    chore: api prefix설정

--- packages/client/src/utility/filter.ts
+++ packages/client/src/utility/filter.ts
@@ -3,6 +3,6 @@ import { Market } from '@/types/market';
 
 export function filterCoin(data: MarketData[], type: Market) {
 	const regex = new RegExp(`^${type}-\\w+`);
-	console.log(data)
+	
 	return data.filter((data) => regex.test(data.market));
 }
