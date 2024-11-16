2fef93a35a0432a1ffc7263ba2c2a6af1176531a
Seonghyeon0409 <t139754@gmail.com>
2024-11-17 00:01:38 +0900

    chore: redis.config 구조 변경

--- packages/server/src/configs/redis.config.ts
+++ packages/server/src/configs/redis.config.ts
@@ -2,24 +2,25 @@ import Redis from 'ioredis';
 
 let redisInstance: Redis | null = null;
 
+export const redisConfig = {
+	host: process.env.REDIS_HOST || '127.0.0.1',
+	port: Number(process.env.REDIS_PORT) || 6379,
+	password: process.env.REDIS_PASSWORD || undefined,
+	db: Number(process.env.REDIS_DB) || 0,
+};
+
 export default function getRedisClient(): Redis {
-  if (!redisInstance) {
-    redisInstance = new Redis({
-      host: process.env.REDIS_HOST || '127.0.0.1',
-      port: Number(process.env.REDIS_PORT) || 6379,
-      password: process.env.REDIS_PASSWORD || undefined,
-      db: Number(process.env.REDIS_DB) || 0,
-    });
+	if (!redisInstance) {
+		redisInstance = new Redis(redisConfig);
 
-    redisInstance.on('connect', () => {
-      console.log('Redis 연결 성공!');
-    });
+		redisInstance.on('connect', () => {
+			console.log('Redis 연결 성공!');
+		});
 
-    redisInstance.on('error', (error) => {
-      console.error('Redis 연결 오류:', error);
-    });
-  }
+		redisInstance.on('error', (error) => {
+			console.error('Redis 연결 오류:', error);
+		});
+	}
 
-  return redisInstance;
+	return redisInstance;
 }
-
