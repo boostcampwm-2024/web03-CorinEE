fe7e0f371456a82a6af98201d60f6877d447c4fb
Seonghyeon0409 <t139754@gmail.com>
2024-11-17 01:26:40 +0900

    chore: redis.config.ts 수정

--- packages/server/src/configs/redis.config.ts
+++ packages/server/src/configs/redis.config.ts
@@ -1,8 +1,8 @@
-export function getRedisConfig() { 
-  return {
-	host: process.env.REDIS_HOST || '127.0.0.1',
-	port: Number(process.env.REDIS_PORT) || 3308,
-	password: process.env.REDIS_PASSWORD || undefined,
-	db: Number(process.env.REDIS_DB) || 0,
-  }
-};
+export function getRedisConfig() {
+	return {
+		host: process.env.REDIS_HOST || '127.0.0.1',
+		port: Number(process.env.REDIS_PORT) || 6379,
+		password: process.env.REDIS_PASSWORD || undefined,
+		db: Number(process.env.REDIS_DB) || 0,
+	};
+}
