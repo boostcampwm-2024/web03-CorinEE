f4ec165621b17c067f5e06d8704e3c91da76d330
Seonghyeon0409 <t139754@gmail.com>
2024-11-16 23:58:34 +0900

    feat: redis config 작성 및 ssh-tunnel 구조변경

---
+++ packages/server/src/configs/ssh-tunnel.ts
@@ -0,0 +1,80 @@
+import { createTunnel } from 'tunnel-ssh';
+
+export async function setupSshTunnel(): Promise<void> {
+  const env = process.env.NODE_ENV || 'development';
+  
+  if (env !== 'development') {
+    console.log('SSH 터널링은 개발 환경에서만 활성화됩니다.');
+    return;
+  }
+	try {
+    await Promise.all([tunnelMySQL(), tunnelRedis()]);
+    console.log('MySQL 및 Redis 터널링 설정 완료');
+  } catch (error) {
+    console.error('SSH 터널링 설정 중 오류 발생:', error);
+    throw error;
+  }
+}
+
+function tunnelMySQL(): Promise<void> {
+	return tunnel(
+		{
+			host: process.env.SSH_HOST,
+			port: Number(process.env.SSH_PORT),
+			username: process.env.SSH_USER,
+			password: process.env.SSH_PASSWORD,
+		},
+		{
+			srcAddr: process.env.DB_HOST,
+			srcPort: Number(process.env.DB_PORT),
+			dstAddr: process.env.SSH_DB_HOST,
+			dstPort: Number(process.env.SSH_DB_TUNNEL_PORT),
+		},
+	);
+}
+
+function tunnelRedis(): Promise<void> {
+	return tunnel(
+		{
+			host: process.env.SSH_HOST,
+			port: Number(process.env.SSH_PORT),
+			username: process.env.SSH_USER,
+			password: process.env.SSH_PASSWORD,
+		},
+		{
+			srcAddr: process.env.REDIS_HOST,
+			srcPort: Number(process.env.REDIS_PORT),
+			dstAddr: process.env.SSH_REDIS_HOST,
+			dstPort: Number(process.env.SSH_REDIS_TUNNEL_PORT),
+		},
+	);
+}
+
+function tunnel(
+	sshOptions: Record<string, any>,
+	forwardOptions: {
+		srcAddr: string;
+		srcPort: number;
+		dstAddr: string;
+		dstPort: number;
+	},
+): Promise<void> {
+	return new Promise((resolve, reject) => {
+		createTunnel(
+			{ autoClose: true },
+			{ port: forwardOptions.srcPort },
+			sshOptions,
+			forwardOptions,
+		)
+			.then((server) => {
+				console.log(
+					`터널링 성공: ${forwardOptions.dstAddr}:${forwardOptions.dstPort}`,
+				);
+				resolve();
+			})
+			.catch((error) => {
+				console.error('터널 생성 중 오류 발생:', error);
+				reject(error);
+			});
+	});
+}
--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,40 +1,48 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { SwaggerModule, DocumentBuilder, SwaggerCustomOptions } from '@nestjs/swagger';
+import {
+  SwaggerModule,
+  DocumentBuilder,
+  SwaggerCustomOptions,
+} from '@nestjs/swagger';
+import { config } from 'dotenv';
+
+config();
+
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule);
 
-	app.enableCors({
-		origin: true,
-		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-		credentials: true,
-	});
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
 
-	const config = new DocumentBuilder()
-    	.setTitle('CorinEE API example')
-    	.setDescription('CorinEE API description')
-    	.setVersion('1.0')
-    	.addTag('corinee')
-		.addBearerAuth(
-			{
-			  type: 'http',
-			  scheme: 'bearer',
-			  name: 'Authorization',
-			  in: 'header',
-			},
-			'access-token',
-		)
-    	.build();
-	const customOptions: SwaggerCustomOptions = {
-		swaggerOptions: {
-			persistAuthorization: true,
-		},
-	};
-  	const documentFactory = () => SwaggerModule.createDocument(app, config);
-  	SwaggerModule.setup('api', app, documentFactory);
+  const config = new DocumentBuilder()
+    .setTitle('CorinEE API example')
+    .setDescription('CorinEE API description')
+    .setVersion('1.0')
+    .addTag('corinee')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        name: 'Authorization',
+        in: 'header',
+      },
+      'access-token',
+    )
+    .build();
+  const customOptions: SwaggerCustomOptions = {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  };
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
-	app.setGlobalPrefix('api');
+  app.setGlobalPrefix('api');
 
-	await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
