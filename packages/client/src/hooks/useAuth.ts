fe6d24d5489400ac61eb1f23d08a217b5dd1958c
qwer0114 <skyok203@naver.com>
2024-11-16 19:24:22 +0900

    feat: 로그인 커스텀 훅 정의

---
+++ packages/client/src/hooks/useAuth.ts
@@ -0,0 +1,19 @@
+import { adminLogin } from '@/api/auth';
+import { useAuthStore } from '@/store/authStore';
+import { setCookie } from '@/utility/cookies';
+import { useMutation } from '@tanstack/react-query';
+
+export function useAuth() {
+	const checkAuth = useAuthStore((state) => state.checkAuth);
+	const useAdminLogin = useMutation({
+		mutationFn: adminLogin,
+		onSuccess: ({ access_token }: { access_token: string }) => {
+			setCookie('access_token', access_token, { maxAge: 24 * 60 * 60 });
+			checkAuth();
+		},
+		onError: (error) => {
+			console.error(error);
+		},
+	});
+	return useAdminLogin;
+}
