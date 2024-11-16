ec299f1b9082fa371913dbd928d5877ad97fa4a4
qwer0114 <skyok203@naver.com>
2024-11-16 19:22:12 +0900

    feat: CookieProvider 설정

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,13 +1,16 @@
 import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from '@/App';
 import { ThemeProvider } from '@material-tailwind/react';
 import { BrowserRouter } from 'react-router-dom';
+import { CookiesProvider } from 'react-cookie';
+import './index.css';
+import App from '@/App';
 
 createRoot(document.getElementById('root')!).render(
 	<ThemeProvider>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
+		<CookiesProvider>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</CookiesProvider>
 	</ThemeProvider>,
 );
