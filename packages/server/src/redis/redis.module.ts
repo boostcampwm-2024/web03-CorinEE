74511d692d7aac3adf251fc4572440687e8e4483
Seonghyeon0409 <t139754@gmail.com>
2024-11-17 14:33:12 +0900

    refactor: redis 분리

--- packages/server/src/redis/redis.module.ts
+++ packages/server/src/redis/redis.module.ts
@@ -2,23 +2,36 @@ import { Module, Global } from '@nestjs/common';
 import Redis from 'ioredis';
 import { getRedisConfig } from 'src/configs/redis.config';
 import { RedisRepository } from './redis.repository';
-import { RedisController } from './redis.controller';
 
 @Global()
 @Module({
-  providers: [
-    {
-      provide: 'REDIS_CLIENT',
-      useFactory: () => {
-        const client = new Redis(getRedisConfig());
-        client.on('connect', () => console.log('Redis 연결 성공'));
-        client.on('error', (error) => console.error('Redis 연결 실패:', error));
-        return client;
-      },
-    },
-    RedisRepository
-  ],
-  exports: ['REDIS_CLIENT', RedisRepository],
-  controllers: [RedisController],
+	providers: [
+		{
+			provide: 'TRADE_REDIS_CLIENT', // 트레이드용 Redis 클라이언트
+			useFactory: () => {
+				const config = getRedisConfig();
+				const client = new Redis({ ...config, db: 1 }); // DB 1로 설정
+				client.on('connect', () => console.log('트레이드용 Redis 연결 성공'));
+				client.on('error', (error) =>
+					console.error('트레이드용 Redis 연결 실패:', error),
+				);
+				return client;
+			},
+		},
+		{
+			provide: 'AUTH_REDIS_CLIENT', // Auth용 Redis 클라이언트
+			useFactory: () => {
+				const config = getRedisConfig();
+				const client = new Redis({ ...config, db: 2 }); // DB 2로 설정
+				client.on('connect', () => console.log('Auth용 Redis 연결 성공'));
+				client.on('error', (error) =>
+					console.error('Auth용 Redis 연결 실패:', error),
+				);
+				return client;
+			},
+		},
+		RedisRepository,
+	],
+	exports: ['TRADE_REDIS_CLIENT', 'AUTH_REDIS_CLIENT', RedisRepository],
 })
 export class RedisModule {}
