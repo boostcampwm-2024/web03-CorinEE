e4a5a326749fb5e22a463b7267e39986b3a75e66
qwer0114 <skyok203@naver.com>
2024-11-17 21:30:17 +0900

    feat: 소수점 입력 가능 기능 추가

--- packages/client/src/pages/trade/components/order_form/common/OrderInput.tsx
+++ packages/client/src/pages/trade/components/order_form/common/OrderInput.tsx
@@ -1,31 +1,38 @@
 type OrderInputProps = {
 	label: string;
-	value: number;
-	onChange: (value: number) => void;
+	value: string | undefined;
+	onChange: (value: string) => void;
 	placeholder?: string;
 };
 
 function OrderInput({ label, value, onChange, placeholder }: OrderInputProps) {
 	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-		const numericValue = e.target.value.replace(/[^\d]/g, '');
+		const newValue = e.target.value;
 
-		if (numericValue === '') {
-			onChange(0);
+		if (newValue.startsWith('.')) {
+			const numericValue = `0${newValue}`;
+			onChange(numericValue);
 			return;
 		}
-		onChange(Number(numericValue));
+
+		const numericValue = newValue.replace(/[^\d.]/g, '');
+
+		const dots = numericValue.split('.').length - 1;
+		if (dots > 1) return;
+
+		onChange(numericValue);
 	};
 
 	return (
 		<label className="flex flex-col gap-1">
 			<span>{label}</span>
 			<input
-				className="w-full border-2 border-solid border-gray-300 rounded-md focus:outline-blue-500 p-1  hover:border-blue-300 transition-colors duration-200"
+				className="w-full border-2 border-solid border-gray-300 rounded-md focus:outline-blue-500 p-1 hover:border-blue-300 transition-colors duration-200"
 				type="text"
-				value={value.toLocaleString()}
+				value={value}
 				onChange={handleChange}
 				placeholder={placeholder}
-				inputMode="numeric"
+				inputMode="decimal"
 			/>
 		</label>
 	);
