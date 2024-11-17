33b8a1c27897e1c151dc6e060659e95f567b4d5f
qwer0114 <skyok203@naver.com>
2024-11-17 21:29:54 +0900

    feat: 버튼 클릭시 api 호출 기능 추가

--- packages/client/src/pages/trade/components/order_form/common/PercentageButtons.tsx
+++ packages/client/src/pages/trade/components/order_form/common/PercentageButtons.tsx
@@ -1,8 +1,40 @@
-function PercentageButtons() {
+import { usePercentageBuy } from '@/hooks/usePercentageBuy';
+import { Dispatch, SetStateAction, useState, useEffect } from 'react';
+
+type PercentageButtonsProps = {
+	price: string;
+	setQuantity: Dispatch<SetStateAction<string>>;
+};
+
+function PercentageButtons({ price, setQuantity }: PercentageButtonsProps) {
+	const [percent, setPercent] = useState<number>();
+	const { data, refetch } = usePercentageBuy({
+		moneyType: 'KRW',
+		percent: percent,
+	});
+
+	const handlePercentClick = (text: string) => {
+		let selectedPercent: number;
+		if (text === '최대') {
+			selectedPercent = 100;
+		} else {
+			selectedPercent = parseInt(text);
+		}
+		setPercent(selectedPercent);
+		refetch();
+	};
+
+	useEffect(() => {
+		if (data) {
+			setQuantity((data / Number(price)).toFixed(8));
+		}
+	}, [data]);
+
 	return (
 		<div className="grid grid-cols-4 gap-2">
 			{['10%', '25%', '50%', '최대'].map((text) => (
 				<button
+					onClick={() => handlePercentClick(text)}
 					key={text}
 					type="button"
 					className="px-2 py-1.5 border border-solid border-gray-400 rounded-md hover:bg-gray-200"
