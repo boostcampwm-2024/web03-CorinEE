a9472ac93e5c06cccdf9bba252f25337358b3b2c
qwer0114 <skyok203@naver.com>
2024-11-18 14:51:10 +0900

    feat: 수량 입력 안했을 시 에러 메시지 추가

--- packages/client/src/pages/trade/components/order_form/OrderBuyForm.tsx
+++ packages/client/src/pages/trade/components/order_form/OrderBuyForm.tsx
@@ -1,25 +1,41 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import OrderInput from '@/pages/trade/components/order_form/common/OrderInput';
 import OrderSubmitButton from '@/pages/trade/components/order_form/common/OrderSubmitButton';
 import PercentageButtons from '@/pages/trade/components/order_form/common/PercentageButtons';
 import NotLogin from '@/components/NotLogin';
 import { useAuthStore } from '@/store/authStore';
-import { usePercentageBuy } from '@/hooks/usePercentageBuy';
 import { calculateTotalPrice } from '@/utility/order';
+import { FormEvent } from 'react';
+
 function OrderBuyForm({ currentPrice }: { currentPrice: number }) {
 	const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
 	const [price, setPrice] = useState(String(currentPrice));
 	const [quantity, setQuantity] = useState<string>('');
-	const { data: accountBalance } = usePercentageBuy({
-		moneyType: 'KRW',
-		percent: 100,
-		type: 'buy',
-	});
+	const [quantityErrorMessage, setQuantityErrorMessage] = useState<string>('');
+
+	useEffect(() => {
+		if (quantityErrorMessage) {
+			const timer = setTimeout(() => {
+				setQuantityErrorMessage('');
+			}, 1500);
+
+			return () => clearTimeout(timer);
+		}
+	}, [quantityErrorMessage]);
 
 	if (!isAuthenticated) return <NotLogin size="md" />;
+
+	const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		if (quantity === '' || !quantity) {
+			setQuantityErrorMessage('수량을 입력해주세요');
+			return;
+		}
+	};
+
 	return (
 		<div className="text-black font-normal text-sm">
-			<form>
+			<form onSubmit={handleSubmit}>
 				<div className="flex flex-col gap-3 mb-10">
 					<OrderInput
 						label="매수 가격(KRW)"
@@ -31,6 +47,7 @@ function OrderBuyForm({ currentPrice }: { currentPrice: number }) {
 						value={quantity}
 						onChange={setQuantity}
 						placeholder="0"
+						errorMessage={quantityErrorMessage}
 					/>
 					<PercentageButtons
 						price={price}
@@ -38,13 +55,9 @@ function OrderBuyForm({ currentPrice }: { currentPrice: number }) {
 						type="buy"
 					/>
 				</div>
-				<div className="flex justify-between pt-5 border-t border-solid border-gray-500">
-					<span>구매가능 금액</span>
-					<span>{accountBalance?.toLocaleString()}원</span>
-				</div>
 				<div className="flex justify-between mt-5">
 					<span>총 주문 금액</span>
-					<span>{calculateTotalPrice(price, quantity)}원</span>
+					<span>{calculateTotalPrice(price, quantity).toLocaleString()}원</span>
 				</div>
 				<OrderSubmitButton type="buy" />
 			</form>
