e7f2a86283184524a635c245e76dc99e626a4b6f
qwer0114 <skyok203@naver.com>
2024-11-18 14:49:24 +0900

    feat: logout api 추가

--- packages/client/src/api/auth.ts
+++ packages/client/src/api/auth.ts
@@ -4,3 +4,12 @@ export async function guestLogin() {
 	const response = await instance.post('/auth/guest-login');
 	return response.data;
 }
+
+export async function logout(token: string) {
+	const response = await instance.delete('/auth/logout', {
+		headers: {
+			Authorization: `Bearer ${token}`,
+		},
+	});
+	return response.data;
+}
