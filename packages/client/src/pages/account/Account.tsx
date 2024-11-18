8cd08e4db1a9a470ed0d08aff88f8721b32b200f
Jun Hee Jung <wjdtka1325@naver.com>
2024-11-18 18:15:41 +0900

    feat: account 카테고리 레이아웃 구현

--- packages/client/src/pages/account/Account.tsx
+++ packages/client/src/pages/account/Account.tsx
@@ -1,5 +1,37 @@
+import AccountCategories from '@/pages/account/AccountCategory';
+import { AccountCategory } from '@/types/category';
+import { useState } from 'react';
+
 function Account() {
-	return <div>Account Page</div>;
+	const [currentAccountCategory, setCurrentAccountCategory] =
+		useState<AccountCategory>('BALANCE');
+
+	const categoryInfo = {
+		BALANCE: { text: '보유자산' },
+		HISTORY: { text: '거래내역' },
+		WAIT_ORDERS: { text: '미체결' },
+	};
+
+	const handleCategory = (accountCategory: AccountCategory) => {
+		setCurrentAccountCategory(accountCategory);
+	};
+
+	return (
+		<>
+		<div className="flex rounded-md">
+			{Object.entries(categoryInfo).map(([category, info]) => (
+				<AccountCategories
+					key={category}
+					text={info.text}
+					isActive={currentAccountCategory === category}
+					category={category as AccountCategory}
+					handleCategory={handleCategory}
+				/>
+			))}
+		</div>
+		
+		</>
+	);
 }
 
 export default Account;
