c910301745ceb14b91afae2342aac6f05ef41549
qwer0114 <skyok203@naver.com>
2024-11-18 11:23:06 +0900

    refactor: guest-login 으로 수정

--- packages/client/src/hooks/useAuth.ts
+++ packages/client/src/hooks/useAuth.ts
@@ -1,12 +1,12 @@
-import { adminLogin } from '@/api/auth';
+import { guestLogin } from '@/api/auth';
 import { useAuthStore } from '@/store/authStore';
 import { setCookie } from '@/utility/cookies';
 import { useMutation } from '@tanstack/react-query';
 
 export function useAuth() {
 	const checkAuth = useAuthStore((state) => state.checkAuth);
-	const useAdminLogin = useMutation({
-		mutationFn: adminLogin,
+	const useGuestLogin = useMutation({
+		mutationFn: guestLogin,
 		onSuccess: ({ access_token }: { access_token: string }) => {
 			setCookie('access_token', access_token, {
 				path: '/',
@@ -19,5 +19,5 @@ export function useAuth() {
 			console.error(error);
 		},
 	});
-	return useAdminLogin;
+	return useGuestLogin;
 }
