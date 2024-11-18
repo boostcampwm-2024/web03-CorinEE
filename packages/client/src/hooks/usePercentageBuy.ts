4845c535c808bebad8b8e05488959860f3e35e74
qwer0114 <skyok203@naver.com>
2024-11-18 12:36:31 +0900

    refactor: 매수/매도 경우 분리

--- packages/client/src/hooks/usePercentageBuy.ts
+++ packages/client/src/hooks/usePercentageBuy.ts
@@ -5,17 +5,20 @@ import { Market } from '@/types/market';
 type CalculateAPI = {
 	moneyType: Market;
 	percent: number | undefined;
+	type: 'buy' | 'sell';
 };
 
-export function usePercentageBuy({ moneyType, percent }: CalculateAPI) {
+export function usePercentageBuy({ moneyType, percent, type }: CalculateAPI) {
 	const QUERY_KEY = 'percentage_buy';
 	const token = getCookie('access_token');
 	const { data, isLoading, refetch } = useQuery({
 		queryFn: () => {
 			if (percent === undefined) return null;
-			return calculatePercentageBuy({ moneyType, percent }, token);
+			if (type === 'buy')
+				return calculatePercentageBuy({ moneyType, percent }, token);
+			else if (type === 'sell') return null;
 		},
-		queryKey: [QUERY_KEY, percent],
+		queryKey: [QUERY_KEY, percent, type],
 		enabled: percent !== undefined,
 	});
 
