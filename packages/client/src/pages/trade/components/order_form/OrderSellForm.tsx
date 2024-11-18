71d2498e5c92281c7647d7b42bf7d380bc619f59
qwer0114 <skyok203@naver.com>
2024-11-18 15:35:47 +0900

    refactor: localeString 으로 보기 좋게 수정

--- packages/client/src/pages/trade/components/order_form/OrderSellForm.tsx
+++ packages/client/src/pages/trade/components/order_form/OrderSellForm.tsx
@@ -3,21 +3,43 @@ import OrderInput from '@/pages/trade/components/order_form/common/OrderInput';
 import OrderSubmitButton from '@/pages/trade/components/order_form/common/OrderSubmitButton';
 import PercentageButtons from '@/pages/trade/components/order_form/common/PercentageButtons';
 import { useAuthStore } from '@/store/authStore';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { calculateTotalPrice } from '@/utility/order';
+import { FormEvent } from 'react';
+
 function OrderSellForm({ currentPrice }: { currentPrice: number }) {
 	const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
 	const [price, setPrice] = useState(String(currentPrice));
 	const [quantity, setQuantity] = useState<string>('');
+	const [quantityErrorMessage, setQuantityErrorMessage] = useState<string>('');
+
+	useEffect(() => {
+		if (quantityErrorMessage) {
+			const timer = setTimeout(() => {
+				setQuantityErrorMessage('');
+			}, 1500);
+
+			return () => clearTimeout(timer);
+		}
+	}, [quantityErrorMessage]);
 
 	if (!isAuthenticated) return <NotLogin size="md" />;
+
+	const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		if (quantity === '' || !quantity) {
+			setQuantityErrorMessage('수량을 입력해주세요');
+			return;
+		}
+	};
+
 	return (
 		<div className="text-black font-normal text-sm">
-			<form>
+			<form onSubmit={handleSubmit}>
 				<div className="flex flex-col gap-3">
 					<OrderInput
 						label="매도 가격(KRW)"
-						value={price}
+						value={Number(price).toLocaleString()}
 						onChange={setPrice}
 					/>
 					<OrderInput
@@ -25,6 +47,7 @@ function OrderSellForm({ currentPrice }: { currentPrice: number }) {
 						value={quantity}
 						onChange={setQuantity}
 						placeholder="0"
+						errorMessage={quantityErrorMessage}
 					/>
 					<PercentageButtons
 						price={price}
