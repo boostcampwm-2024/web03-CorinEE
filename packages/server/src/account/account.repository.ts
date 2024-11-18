586ccccc2a615c2d5f6132a64b56d05b008c55f5
Seonghyeon0409 <t139754@gmail.com>
2024-11-18 11:08:14 +0900

    chore: lint 적용 및 컨벤션 수정

--- packages/server/src/account/account.repository.ts
+++ packages/server/src/account/account.repository.ts
@@ -5,18 +5,18 @@ import { User } from 'src/auth/user.entity';
 
 @Injectable()
 export class AccountRepository extends Repository<Account> {
-	constructor(private dataSource: DataSource) {
-		super(Account, dataSource.createEntityManager());
-	}
-	async createAccountForAdmin(adminUser: User) {
-		const account = new Account();
-		account.KRW = 300000000;
-		account.USDT = 300000;
-		account.BTC = 10;
-		account.user = adminUser;
-		await this.save(account);
-		console.log('admin 계정에 Account가 성공적으로 생성되었습니다.');
-	}
+  constructor(private dataSource: DataSource) {
+    super(Account, dataSource.createEntityManager());
+  }
+  async createAccountForAdmin(adminUser: User) {
+    const account = new Account();
+    account.KRW = 300000000;
+    account.USDT = 300000;
+    account.BTC = 10;
+    account.user = adminUser;
+    await this.save(account);
+    console.log('admin 계정에 Account가 성공적으로 생성되었습니다.');
+  }
   async getMyMoney(user, moneyType: string) {
     const account = await this.findOne({
       where: { user: { id: user.userId } },
@@ -25,12 +25,12 @@ export class AccountRepository extends Repository<Account> {
     if (!account[moneyType]) return 0;
     return account[moneyType];
   }
-  async succesBuy(buyDto,accountBalance,accountId,queryRunner){
-	const updateData = {
-		accountId: accountId,
-		[buyDto.typeGiven]: accountBalance,
-	};
+  async succesBuy(buyDto, accountBalance, accountId, queryRunner) {
+    const updateData = {
+      accountId: accountId,
+      [buyDto.typeGiven]: accountBalance,
+    };
 
-	await queryRunner.manager.save(Account,updateData)
+    await queryRunner.manager.save(Account, updateData);
   }
 }
--- packages/server/src/upbit/sse.service.ts
+++ packages/server/src/upbit/sse.service.ts
@@ -5,72 +5,75 @@ import { CoinDataUpdaterService } from './coin-data-updater.service';
 
 @Injectable()
 export class SseService implements OnModuleDestroy {
-	private coinTickerStream$ = new Subject<any>();
-	private orderbookStream$ = new Subject<any>();
-	private coinTickerDestroy$ = new Subject<void>();
-	private orderBookDestroy$ = new Subject<void>();
+  private coinTickerStream$ = new Subject<any>();
+  private orderbookStream$ = new Subject<any>();
+  private coinTickerDestroy$ = new Subject<void>();
+  private orderBookDestroy$ = new Subject<void>();
 
-	constructor(
-		private readonly coinDataUpdaterService: CoinDataUpdaterService,
-	) {}
+  constructor(
+    private readonly coinDataUpdaterService: CoinDataUpdaterService,
+  ) {}
 
-	coinTickerSendEvent(data: any) {
-		this.coinTickerStream$.next(data);
-	}
-	orderbookSendEvent(data: any) {
-		this.orderbookStream$.next(data);
-	}
+  coinTickerSendEvent(data: any) {
+    this.coinTickerStream$.next(data);
+  }
+  orderbookSendEvent(data: any) {
+    this.orderbookStream$.next(data);
+  }
 
-	initPriceStream(coins, dto: Function) {
-		const events: MessageEvent[] = [];
-		if (coins && typeof coins === 'string') {
-			coins = [coins];
-		}
-		coins.forEach(async (coin) => {
-			let coinLatestInfo = this.coinDataUpdaterService.getCoinLatestInfo()
-			while (coinLatestInfo.size === 0 || coinLatestInfo.get(coin) === undefined) {
-				await new Promise((resolve) => setTimeout(resolve, 100));
-				coinLatestInfo = this.coinDataUpdaterService.getCoinLatestInfo();
-			}
+  initPriceStream(coins, dto: Function) {
+    const events: MessageEvent[] = [];
+    if (coins && typeof coins === 'string') {
+      coins = [coins];
+    }
+    coins.forEach(async (coin) => {
+      let coinLatestInfo = this.coinDataUpdaterService.getCoinLatestInfo();
+      while (
+        coinLatestInfo.size === 0 ||
+        coinLatestInfo.get(coin) === undefined
+      ) {
+        await new Promise((resolve) => setTimeout(resolve, 100));
+        coinLatestInfo = this.coinDataUpdaterService.getCoinLatestInfo();
+      }
 
-			const initData = coinLatestInfo.get(coin);
-			const setDto = dto(initData);
-			const msgEvent = new MessageEvent('price-update', {
-				data: JSON.stringify(setDto),
-			}) as MessageEvent;
+      const initData = coinLatestInfo.get(coin);
+      const setDto = dto(initData);
+      const msgEvent = new MessageEvent('price-update', {
+        data: JSON.stringify(setDto),
+      }) as MessageEvent;
 
-			events.push(msgEvent);
-		});
+      events.push(msgEvent);
+    });
 
-		return events;
-	}
-	getPriceUpdatesStream(coins, dto: Function): Observable<MessageEvent> {
-		return this.coinTickerStream$.asObservable().pipe(
-			takeUntil(this.coinTickerDestroy$),
-			filter((data) => coins.includes(data.code)),
-			map((data) => {
-				const setDto = dto(data);
-				return new MessageEvent('price-update', {
-					data: JSON.stringify(setDto),
-				}) as MessageEvent;
-			}),
-		);
-	}
+    return events;
+  }
+  getPriceUpdatesStream(coins, dto: Function): Observable<MessageEvent> {
+    return this.coinTickerStream$.asObservable().pipe(
+      takeUntil(this.coinTickerDestroy$),
+      filter((data) => coins.includes(data.code)),
+      map((data) => {
+        const setDto = dto(data);
+        return new MessageEvent('price-update', {
+          data: JSON.stringify(setDto),
+        }) as MessageEvent;
+      }),
+    );
+  }
 
-	getOrderbookUpdatesStream(coins, dto: Function): Observable<MessageEvent> {
-		return this.orderbookStream$.asObservable().pipe(
-			takeUntil(this.orderBookDestroy$),
-			filter((data) => coins.includes(data.code)),
-			map((data) => {
-				const setDto = dto(data);
-				return new MessageEvent('orderbook-update', {
-					data: JSON.stringify(setDto),
-				}) as MessageEvent;
-			}),
-		);
-	}
-	onModuleDestroy() {
-		this.coinTickerDestroy$.next();
-		this.orderBookDestroy$.next();
-	}
+  getOrderbookUpdatesStream(coins, dto: Function): Observable<MessageEvent> {
+    return this.orderbookStream$.asObservable().pipe(
+      takeUntil(this.orderBookDestroy$),
+      filter((data) => coins.includes(data.code)),
+      map((data) => {
+        const setDto = dto(data);
+        return new MessageEvent('orderbook-update', {
+          data: JSON.stringify(setDto),
+        }) as MessageEvent;
+      }),
+    );
+  }
+  onModuleDestroy() {
+    this.coinTickerDestroy$.next();
+    this.orderBookDestroy$.next();
+  }
 }
