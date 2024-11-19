55ca8a85ca144ffd70e518aa73f9c10d9ce05863
qwer0114 <skyok203@naver.com>
2024-11-19 13:59:59 +0900

    refactor: 차트 useSuspenseInfinityQuery로 무한 스크롤 구현

--- packages/client/src/hooks/usePeriodChart.ts
+++ packages/client/src/hooks/usePeriodChart.ts
@@ -1,14 +1,38 @@
 import { getCandleByPeriod } from '@/api/market';
-import { CandlePeriod } from '@/types/chart';
-import { useSuspenseQuery } from '@tanstack/react-query';
+import { Candle, CandlePeriod, InfiniteCandle } from '@/types/chart';
+import { useSuspenseInfiniteQuery } from '@tanstack/react-query';
 
-export function usePeriodChart(market: string, period: CandlePeriod) {
-	const { data } = useSuspenseQuery({
-		queryKey: [market, period],
-		queryFn: () => getCandleByPeriod(market, period),
-		staleTime: 1000 * 60, // 1분
-		gcTime: 1000 * 60 * 5, // 5분
+export function usePeriodChart(
+	market: string,
+	period: CandlePeriod,
+	minute?: number,
+) {
+	const { data, fetchNextPage, hasNextPage } = useSuspenseInfiniteQuery<
+		Candle[],
+		Error,
+		InfiniteCandle,
+		[string, string, CandlePeriod, number?],
+		string | undefined
+	>({
+		queryKey: ['candles', market, period, minute],
+		queryFn: ({ pageParam }) => {
+			return getCandleByPeriod(market, period, pageParam, minute);
+		},
+		getNextPageParam: (lastPage) => {
+			if (lastPage.length === 0) return undefined;
+			const oldestCandle = lastPage[lastPage.length - 1];
+			return oldestCandle?.candle_date_time_utc;
+		},
+		initialPageParam: undefined,
+		select: (data) => ({
+			candles: data.pages.flat(),
+			hasNextPage: data.pages[data.pages.length - 1]?.length === 200,
+		}),
 	});
 
-	return { data };
+	return {
+		data,
+		fetchNextPage,
+		hasNextPage,
+	};
 }
