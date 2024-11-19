75828427472bf80b5fe4bf4e7f6b073b89edd990
SeungGwa123 <skwan123@naver.com>
2024-11-19 19:15:41 +0900

    feat: 마이페이지 api 작성

--- packages/server/src/account/account.module.ts
+++ packages/server/src/account/account.module.ts
@@ -3,8 +3,17 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Account } from './account.entity';
 import { AccountRepository } from './account.repository';
 import { User } from 'src/auth/user.entity';
+import { AccountController } from './account.controller';
+import { AuthService } from '@src/auth/auth.service';
+import { AccountService } from './account.service';
+import { AssetRepository } from '@src/asset/asset.repository';
 @Module({
   imports: [TypeOrmModule.forFeature([Account, User])],
-  providers: [AccountRepository],
+  controllers:[AccountController],
+  providers: [
+    AccountRepository,
+    AccountService,
+    AssetRepository
+  ],
 })
 export class AccountModule {}
---
+++ packages/server/src/account/account.service.ts
@@ -0,0 +1,55 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { AssetRepository } from '@src/asset/asset.repository';
+import { UPBIT_IMAGE_URL } from 'common/upbit';
+import { AccountRepository } from 'src/account/account.repository';
+import { MyAccountDto } from './dtos/myAccount.dto';
+import { CoinDataUpdaterService } from '@src/upbit/coin-data-updater.service';
+
+
+@Injectable()
+export class AccountService {
+	constructor(
+        private accountRepository : AccountRepository,
+        private assetRepository: AssetRepository,
+        private coinDataUpdaterService : CoinDataUpdaterService
+	) {}
+
+    async getMyAccountData(user){
+        const accountData: MyAccountDto = new MyAccountDto();
+
+        const account = await this.accountRepository.findOne({
+            where: {user:{id:user.userId}}
+        })
+        if(!account){
+            return {
+                code: 400,
+                message: "등록되지 않은 사용자입니다."
+            }
+        }
+        const KRW = account.KRW;
+        let total_price = 0;
+        
+        const myCoins = await this.assetRepository.find({
+            where: { account: { id : account.id } }
+        })
+        const coinNameData = this.coinDataUpdaterService.getCoinNameList();
+        const coins = [];
+        myCoins.forEach((myCoin)=>{
+            const name = myCoin.assetName
+            const coin = {
+                img_url : `${UPBIT_IMAGE_URL}${name}.png`,
+                koreanName: coinNameData.get(`KRW-${name}`)||coinNameData.get(`BTC-${name}`)||coinNameData.get(`USDT-${name}`),
+                market: name,
+                quantity: myCoin.quantity,
+                price : myCoin.price,
+                averagePrice : myCoin.price / myCoin.quantity
+            };
+            coins.push(coin)
+            total_price+=myCoin.price
+        })
+        accountData.KRW = KRW;
+        accountData.total_bid = total_price;
+        accountData.coins = coins;
+        return accountData;
+    }
+}
