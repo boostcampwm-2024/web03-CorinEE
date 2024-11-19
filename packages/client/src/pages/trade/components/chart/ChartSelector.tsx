219396c900e7263f6179ab1fa368acaa955c5d59
qwer0114 <skyok203@naver.com>
2024-11-19 14:00:47 +0900

    feat: 분 단위 차트 조회 추가

--- packages/client/src/pages/trade/components/chart/ChartSelector.tsx
+++ packages/client/src/pages/trade/components/chart/ChartSelector.tsx
@@ -1,4 +1,6 @@
+import MinuteDropDown from '@/pages/trade/components/chart/MinuteDropDown';
 import { CandlePeriod } from '@/types/chart';
+import { PERIODS } from '@/constants/chartSelector';
 
 type ChartSelector = {
 	activePeriods: CandlePeriod;
@@ -6,38 +8,27 @@ type ChartSelector = {
 };
 
 function ChartSelector({ activePeriods, handleActivePeriod }: ChartSelector) {
-	const PERIODS: { label: string; id: CandlePeriod }[] = [
-		{
-			label: '일',
-			id: 'days',
-		},
-		{
-			label: '주',
-			id: 'weeks',
-		},
-		{
-			label: '월',
-			id: 'months',
-		},
-		{
-			label: '년',
-			id: 'years',
-		},
-	];
-
 	return (
 		<div className="text-sm flex gap-6 py-2 pl-3 items-center">
 			<span className="font-semibold text-gray-900">차트</span>
 			<div className="flex gap-3">
-				{PERIODS.map(({ label, id }) => (
-					<button
-						key={id}
-						className={`text-gray-900 py-1 px-3 rounded-md ${activePeriods === id ? 'bg-gray-300' : ''} hover:bg-gray-200`}
-						onClick={() => handleActivePeriod(id)}
-					>
-						{label}
-					</button>
-				))}
+				{PERIODS.map(({ label, id }) =>
+					id === 'minutes' ? (
+						<MinuteDropDown
+							key={id}
+							active={activePeriods === id}
+							handleActivePeriod={handleActivePeriod}
+						/>
+					) : (
+						<button
+							key={id}
+							className={`text-gray-900 py-1 px-3 rounded-md ${activePeriods === id ? 'bg-gray-300' : ''} hover:bg-gray-200`}
+							onClick={() => handleActivePeriod(id)}
+						>
+							{label}
+						</button>
+					),
+				)}
 			</div>
 		</div>
 	);
