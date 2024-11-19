ee302c05332d0b040d3629df067ec3e0468f32e6
qwer0114 <skyok203@naver.com>
2024-11-19 22:55:12 +0900

    feat: react-toastfiy 라이브러리 정의

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -4,12 +4,14 @@ import { BrowserRouter } from 'react-router-dom';
 import { CookiesProvider } from 'react-cookie';
 import './index.css';
 import App from '@/App';
+import { ToastContainer } from 'react-toastify';
 
 createRoot(document.getElementById('root')!).render(
 	<ThemeProvider>
 		<CookiesProvider>
 			<BrowserRouter>
 				<App />
+				<ToastContainer />
 			</BrowserRouter>
 		</CookiesProvider>
 	</ThemeProvider>,
