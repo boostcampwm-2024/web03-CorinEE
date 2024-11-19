f0d95046a39ab572db01939c7d6f6363b371bb3d
qwer0114 <skyok203@naver.com>
2024-11-19 22:57:47 +0900

    chore: 불필요한 주석 제거

--- packages/client/src/pages/trade/components/order_book/OrderBook.tsx
+++ packages/client/src/pages/trade/components/order_book/OrderBook.tsx
@@ -9,7 +9,7 @@ type OrderBookProps = {
 };
 
 function OrderBook({ orderBook, currentPrice }: OrderBookProps) {
-	if (!orderBook) return; // 임시 방편 처리
+	if (!orderBook) return;
 	const asks = formatAsks(orderBook);
 	const bids = formatBids(orderBook);
 
