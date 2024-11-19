660b524c60322d78f9e78bc4d7de5ba9b19c5185
SeungGwa123 <skwan123@naver.com>
2024-11-19 17:58:23 +0900

    fix: 매수, 매도 에러 수정 및 퍼센티지 api 에러처리

--- packages/server/src/account/account.repository.ts
+++ packages/server/src/account/account.repository.ts
@@ -1,4 +1,4 @@
-import { DataSource, Repository } from 'typeorm';
+import { DataSource, Repository, QueryRunner } from 'typeorm';
 import { Account } from './account.entity';
 import { Injectable } from '@nestjs/common';
 import { User } from 'src/auth/user.entity';
@@ -12,7 +12,7 @@ export class AccountRepository extends Repository<Account> {
 		const account = new Account();
 		account.KRW = 300000000;
 		account.USDT = 300000;
-		account.BTC = 10;
+		account.BTC = 0;
 		account.user = adminUser;
 		await this.save(account);
 		console.log('admin 계정에 Account가 성공적으로 생성되었습니다.');
@@ -26,15 +26,27 @@ export class AccountRepository extends Repository<Account> {
 		return account[moneyType];
 	}
 	async updateAccountCurrency(typeGiven, accountBalance, accountId, queryRunner) {
-    try{
-      const updateData = {
-        id: accountId,
-        [typeGiven]: accountBalance,
-      };
-
-		await queryRunner.manager.save(Account, updateData);
-    }catch(error){
-      console.log(error)
-    }
+		try{
+			await queryRunner.manager
+				.createQueryBuilder()
+				.update(Account)
+				.set({ 
+					[typeGiven]: accountBalance,
+				})
+				.where('id = :id', { id: accountId })
+				.execute();
+		}catch(error){
+		console.log(error)
+		}
+	}
+	async updateAccountBTC(id, quantity, queryRunner){
+		await queryRunner.manager
+        .createQueryBuilder()
+        .update(Account)
+        .set({ 
+          BTC: quantity,
+        })
+        .where('id = :id', { id: id })
+        .execute();
 	}
 }
