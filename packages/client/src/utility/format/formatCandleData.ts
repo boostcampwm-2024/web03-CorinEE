532ba6804098afb6441c4355e9a2fcc7fcf79dc6
qwer0114 <skyok203@naver.com>
2024-11-19 14:01:33 +0900

    refactor: 차트 시간 한국 시간으로 수정

--- packages/client/src/utility/format/formatCandleData.ts
+++ packages/client/src/utility/format/formatCandleData.ts
@@ -1,9 +1,10 @@
 import { Candle, CandleFormat } from '@/types/chart';
+import { Time } from 'lightweight-charts';
 
 export function formatCandleData(data: Candle[]): CandleFormat[] {
 	return data
 		.map((candle) => ({
-			time: candle.candle_date_time_utc.split('T')[0],
+			time: (new Date(candle.candle_date_time_kst).getTime() / 1000) as Time,
 			open: candle.opening_price,
 			high: candle.high_price,
 			low: candle.low_price,
