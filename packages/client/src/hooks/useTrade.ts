5571620599ba08ff163b8464493770a45c9e9ab7
qwer0114 <skyok203@naver.com>
2024-11-19 22:57:34 +0900

    feat: 매수/매도 커스텀 훅 정의

---
+++ packages/client/src/hooks/useTrade.ts
@@ -0,0 +1,37 @@
+import { trade } from '@/api/trade';
+import { getCookie } from '@/utility/cookies';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { Market } from '@/types/market';
+import { useToast } from '@/hooks/useToast';
+import { AxiosError } from 'axios';
+type TradeAPI = {
+	askType: 'bid' | 'ask';
+	typeGiven: Market | string;
+	typeReceived: Market | string;
+	receivedPrice: number;
+	receivedAmount: number;
+};
+
+export function useTrade(askType: 'bid' | 'ask') {
+	const token = getCookie('access_token');
+	const toast = useToast();
+	const queryClient = useQueryClient();
+	const tradeMutation = useMutation({
+		mutationFn: (params: TradeAPI) => trade(params, token),
+		onSuccess: () => {
+			if (askType == 'bid') {
+				toast.success('매수 주문을 등록했어요');
+				queryClient.refetchQueries({ queryKey: ['MY_ACCOUNT'] });
+			} else toast.success('매도 주문을 등록했어요');
+		},
+		onError: (error: AxiosError) => {
+			if (error.status === 422 && askType == 'bid') {
+				toast.error('계좌 잔액이 부족해요!');
+			} else if (error.status === 422 && askType == 'ask') {
+				toast.error('판매할 수 있는 수량이 부족해요!');
+			}
+		},
+	});
+
+	return tradeMutation;
+}
