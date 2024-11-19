89dc35acd43fbc13dfe95b5e98e1880cee788ae9
qwer0114 <skyok203@naver.com>
2024-11-19 22:56:00 +0900

    feat: 매수/매도 커스텀 훅 정의

--- packages/client/src/api/trade.ts
+++ packages/client/src/api/trade.ts
@@ -2,21 +2,58 @@ import { instance } from '@/api/instance';
 import { Market } from '@/types/market';
 
 type CalculateAPI = {
-	moneyType: Market;
+	askType: 'ask' | 'bid';
+	moneyType: Market | string;
 	percent: number;
 };
 
 export async function calculatePercentageBuy(
-	{ moneyType, percent }: CalculateAPI,
+	{ askType, moneyType, percent }: CalculateAPI,
 	token: string,
 ) {
 	const response = await instance.get(
-		`/trade/calculate-percentage-buy/${moneyType}?percent=${percent}`,
+		`/trade/calculate-percentage-${askType}/${moneyType}?percent=${percent}`,
 		{
 			headers: {
 				Authorization: `Bearer ${token}`,
 			},
 		},
 	);
+	return response.data;
+}
+
+type TradeAPI = {
+	askType: 'bid' | 'ask';
+	typeGiven: Market | string;
+	typeReceived: Market | string;
+	receivedPrice: number;
+	receivedAmount: number;
+};
+
+export async function trade(params: TradeAPI, token: string) {
+	const response = await instance.post(
+		`/trade/${params.askType}`,
+		{
+			typeGiven: params.typeGiven,
+			typeReceived: params.typeReceived,
+			receivedPrice: params.receivedPrice,
+			receivedAmount: params.receivedAmount,
+		},
+		{
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		},
+	);
+
+	return response.data;
+}
+
+export async function checkCoin(coin: string, token: string) {
+	const response = await instance.get(`/trade/check-coinData/${coin}`, {
+		headers: {
+			Authorization: `Bearer ${token}`,
+		},
+	});
 	return response.data;
 }
