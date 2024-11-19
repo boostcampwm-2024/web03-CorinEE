0590e9353378d98cbaba1d305867303a1ace39f9
qwer0114 <skyok203@naver.com>
2024-11-20 00:17:37 +0900

    refactor: Notlogin 구조 개선에 따른 리팩토링

--- packages/client/src/pages/trade/components/order_form/OrderWaitForm.tsx
+++ packages/client/src/pages/trade/components/order_form/OrderWaitForm.tsx
@@ -1,9 +1,4 @@
-import NotLogin from '@/components/NotLogin';
-import { useAuthStore } from '@/store/authStore';
-
 function OrderWaitForm() {
-	const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
-	if (!isAuthenticated) return <NotLogin size="md" />;
 	return <div>order wait</div>;
 }
 
