449e682cf77cb721a757d3cc09334ef73f50c8cb
qwer0114 <skyok203@naver.com>
2024-11-19 22:56:48 +0900

    feat: 코인 존재 여부 api 커스텀 훅 정의

---
+++ packages/client/src/hooks/useCheckCoin.ts
@@ -0,0 +1,13 @@
+import { checkCoin } from '@/api/trade';
+import { getCookie } from '@/utility/cookies';
+import { useSuspenseQuery } from '@tanstack/react-query';
+
+export function useCheckCoin(coin: string) {
+	const token = getCookie('access_token');
+	const { data } = useSuspenseQuery({
+		queryFn: () => checkCoin(coin, token),
+		queryKey: ['checkCoin'],
+	});
+
+	return { data };
+}
