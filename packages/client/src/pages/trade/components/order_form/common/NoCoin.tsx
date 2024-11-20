9624849c08e122859b864eeba95e7a91749b5115
qwer0114 <skyok203@naver.com>
2024-11-20 12:36:38 +0900

    chore: 미보유 코인 애니메이션 변경

--- packages/client/src/pages/trade/components/order_form/common/NoCoin.tsx
+++ packages/client/src/pages/trade/components/order_form/common/NoCoin.tsx
@@ -1,13 +1,17 @@
 import Lottie from 'lottie-react';
-import CoinFlyLottie from '@asset/lotties/CoinFlyLottie.json';
+import Wallet from '@asset/lotties/Wallet.json';
 function NoCoin() {
 	return (
 		<div className="flex flex-col justify-center items-center">
 			<Lottie
 				className="w-64 h-64"
-				animationData={CoinFlyLottie}
+				animationData={Wallet}
 				loop={true}
 				autoPlay={true}
+				rendererSettings={{
+					preserveAspectRatio: 'xMidYMid slice',
+					progressiveLoad: true,
+				}}
 			/>
 			<div className="text-gray-700">판매할 코인이 없어요</div>
 		</div>
