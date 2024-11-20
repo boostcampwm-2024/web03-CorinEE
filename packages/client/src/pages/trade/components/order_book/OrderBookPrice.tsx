1d801ed218076e883dca5a6486614f50031ca9e9
qwer0114 <skyok203@naver.com>
2024-11-20 16:55:22 +0900

    feat: 호가창 클릭 시 매수/매도 가격 변동 로직 추가

--- packages/client/src/pages/trade/components/order_book/OrderBookPrice.tsx
+++ packages/client/src/pages/trade/components/order_book/OrderBookPrice.tsx
@@ -2,9 +2,15 @@ type OrderBookPriceProps = {
 	currentPrice: number;
 	price: number;
 	rate: string;
+	handleSelectPrice: (price: number) => void;
 };
 
-function OrderBookPrice({ currentPrice, price, rate }: OrderBookPriceProps) {
+function OrderBookPrice({
+	currentPrice,
+	price,
+	rate,
+	handleSelectPrice,
+}: OrderBookPriceProps) {
 	const textColor = rate.includes('-') ? 'text-blue-600' : 'text-red-500';
 	const borderStyle =
 		currentPrice === price ? 'border border-black border-solid' : '';
@@ -13,7 +19,12 @@ function OrderBookPrice({ currentPrice, price, rate }: OrderBookPriceProps) {
 		<div
 			className={`col-span-2 text-center cursor-pointer hover:bg-gray-200 ${textColor} ${borderStyle}`}
 		>
-			<span className="text-center flex-1 p-3">{price.toLocaleString()}</span>
+			<span
+				className="text-center flex-1 p-3"
+				onClick={() => handleSelectPrice(price)}
+			>
+				{price.toLocaleString()}
+			</span>
 			<span className="text-xs">{rate}</span>
 		</div>
 	);
