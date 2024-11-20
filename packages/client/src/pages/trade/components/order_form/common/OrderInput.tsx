b2e7e45f1805806b23d6ff44e5f4282345f5118d
qwer0114 <skyok203@naver.com>
2024-11-20 16:26:41 +0900

    refactor: 불필요한 공백 제거

--- packages/client/src/pages/trade/components/order_form/common/OrderInput.tsx
+++ packages/client/src/pages/trade/components/order_form/common/OrderInput.tsx
@@ -15,18 +15,14 @@ function OrderInput({
 }: OrderInputProps) {
 	const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
 		const newValue = e.target.value;
-
 		if (newValue.startsWith('.')) {
 			const numericValue = `0${newValue}`;
 			onChange(numericValue);
 			return;
 		}
-
 		const numericValue = newValue.replace(/[^\d.]/g, '');
-
 		const dots = numericValue.split('.').length - 1;
 		if (dots > 1) return;
-
 		onChange(numericValue);
 	};
 
