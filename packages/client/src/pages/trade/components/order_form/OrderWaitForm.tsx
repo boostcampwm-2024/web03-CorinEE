c9aa3431bfba306b427c9b8fe9d8d4c6ff823ef6
qwer0114 <skyok203@naver.com>
2024-11-20 16:26:24 +0900

    feat: 대기중 상태 코인 컴포넌트 정의

--- packages/client/src/pages/trade/components/order_form/OrderWaitForm.tsx
+++ packages/client/src/pages/trade/components/order_form/OrderWaitForm.tsx
@@ -1,14 +1,29 @@
 import { useMarketParams } from '@/hooks/market/useMarketParams';
 import { usePendingCoin } from '@/hooks/usePendingCoin';
+import { useToast } from '@/hooks/useToast';
 import NoCoin from '@/pages/trade/components/order_form/common/NoCoin';
+import { PendingCoin } from '@/pages/trade/components/order_form/common/PendingCoin';
 
 function OrderWaitForm() {
 	const { market } = useMarketParams();
-	const { data } = usePendingCoin(`${market}`);
+	const { data } = usePendingCoin(market);
+	const toast = useToast();
 
-	if (!data.tradeData.length)
+	const handleCancel = () => {
+		toast.info('개발중인 기능입니다!');
+	};
+
+	if (!data.result.length) {
 		return <NoCoin message="대기중인 주문이 없어요" />;
-	return <div>order wait</div>;
+	}
+
+	return (
+		<div className="space-y-2">
+			{data.result.map((order, index) => (
+				<PendingCoin key={index} order={order} onCancel={handleCancel} />
+			))}
+		</div>
+	);
 }
 
 export default OrderWaitForm;
