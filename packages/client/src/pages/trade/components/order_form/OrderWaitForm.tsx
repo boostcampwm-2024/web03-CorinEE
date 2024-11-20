290a91a223aef50bc606c024b83beca7c3f74d36
qwer0114 <skyok203@naver.com>
2024-11-20 15:41:19 +0900

    feat: 대기중인 코인 api 커스텀 훅 추가

--- packages/client/src/pages/trade/components/order_form/OrderWaitForm.tsx
+++ packages/client/src/pages/trade/components/order_form/OrderWaitForm.tsx
@@ -1,4 +1,13 @@
+import { useMarketParams } from '@/hooks/market/useMarketParams';
+import { usePendingCoin } from '@/hooks/usePendingCoin';
+import NoCoin from '@/pages/trade/components/order_form/common/NoCoin';
+
 function OrderWaitForm() {
+	const { market } = useMarketParams();
+	const { data } = usePendingCoin(`${market}`);
+
+	if (!data.tradeData.length)
+		return <NoCoin message="대기중인 주문이 없어요" />;
 	return <div>order wait</div>;
 }
 
