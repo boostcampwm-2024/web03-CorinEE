2c9be1410ecc9435d5765b3a5b372458f4631104
SeungGwa123 <skwan123@naver.com>
2024-11-20 13:23:29 +0900

    feat: 거래내역 api 작성

--- packages/server/src/trade-history/trade-history.module.ts
+++ packages/server/src/trade-history/trade-history.module.ts
@@ -1,4 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TradeHistoryRepository } from './trade-history.repository';
+import { TradeHistory } from './trade-history.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CoinDataUpdaterService } from '@src/upbit/coin-data-updater.service';
+import { HttpModule } from '@nestjs/axios';
+import { TradeHistoryController } from './trade-history.controller';
+import { TradeHistoryService } from './trade-history.service';
+import { UpbitModule } from '@src/upbit/upbit.module';
 
-@Module({})
+@Module({
+    imports: [TypeOrmModule.forFeature([TradeHistory]), HttpModule, UpbitModule],
+    providers: [
+        TradeHistoryRepository,
+        TradeHistoryService,
+    ],
+    controllers: [TradeHistoryController],
+})
 export class TradehistoryModule {}
