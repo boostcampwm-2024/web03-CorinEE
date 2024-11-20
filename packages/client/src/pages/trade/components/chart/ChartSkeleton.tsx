22f4b1fdfd7a25ecb742cbe6945b0cbb15dd33fb
qwer0114 <skyok203@naver.com>
2024-11-20 23:36:50 +0900

    refactor: 차트 ui 크기에 맞게 변경

--- packages/client/src/pages/trade/components/chart/ChartSkeleton.tsx
+++ packages/client/src/pages/trade/components/chart/ChartSkeleton.tsx
@@ -1,5 +1,7 @@
 function ChartSkeleton() {
-	return <div className="bg-gray-50 w-2/3 rounded-lg min-w-96"></div>;
+	return (
+		<div className="bg-gray-50 min-w-80 rounded-lg flex-[2] overflow-hidden"></div>
+	);
 }
 
 export default ChartSkeleton;
