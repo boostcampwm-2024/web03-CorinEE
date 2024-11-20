aa40769f8458b257d16642b2f1621c286f0666c4
qwer0114 <skyok203@naver.com>
2024-11-20 12:38:55 +0900

    refactor: 호가 창 hover 효과 추가

--- packages/client/src/pages/trade/components/order_book/OrderBookPrice.tsx
+++ packages/client/src/pages/trade/components/order_book/OrderBookPrice.tsx
@@ -10,7 +10,9 @@ function OrderBookPrice({ currentPrice, price, rate }: OrderBookPriceProps) {
 		currentPrice === price ? 'border border-black border-solid' : '';
 
 	return (
-		<div className={`col-span-2 text-center ${textColor} ${borderStyle}`}>
+		<div
+			className={`col-span-2 text-center cursor-pointer hover:bg-gray-200 ${textColor} ${borderStyle}`}
+		>
 			<span className="text-center flex-1 p-3">{price.toLocaleString()}</span>
 			<span className="text-xs">{rate}</span>
 		</div>
