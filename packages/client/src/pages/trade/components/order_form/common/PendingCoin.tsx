d66583aa2b6646b1fde7ec2e51a86a8deacb01d8
qwer0114 <skyok203@naver.com>
2024-11-20 16:26:24 +0900

    feat: 대기중 상태 코인 컴포넌트 정의

---
+++ packages/client/src/pages/trade/components/order_form/common/PendingCoin.tsx
@@ -0,0 +1,39 @@
+interface PendingOrder {
+	tradeType: 'sell' | 'buy';
+	quantity: number;
+	price: number;
+}
+
+interface PendingCoinProps {
+	order: PendingOrder;
+	onCancel: () => void;
+}
+
+export function PendingCoin({ order, onCancel }: PendingCoinProps) {
+	const tradeTypeConfig = {
+		sell: { text: '매도', className: 'text-blue-800' },
+		buy: { text: '매수', className: 'text-red-600' },
+	};
+
+	const { text, className } = tradeTypeConfig[order.tradeType];
+
+	return (
+		<div className="flex justify-between items-center border-b border-solid border-gray-300 py-2">
+			<div>
+				<div className="flex gap-1">
+					<div className={`${className} font-semibold`}>{text}</div>
+					<div>{order.quantity}개</div>
+				</div>
+				<div className="text-sm text-gray-700">
+					코인 당 {order.price.toLocaleString()}원
+				</div>
+			</div>
+			<button
+				className="font-semibold px-2 py-1 text-blue-900 rounded-md text-sm bg-blue-100 hover:bg-blue-200"
+				onClick={onCancel}
+			>
+				취소
+			</button>
+		</div>
+	);
+}
