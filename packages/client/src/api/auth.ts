2602c85d5dbdd6cb5fefb38d62005bb77c3672b1
qwer0114 <skyok203@naver.com>
2024-11-20 17:08:28 +0900

    feat: auth api 타입 지정

--- packages/client/src/api/auth.ts
+++ packages/client/src/api/auth.ts
@@ -1,11 +1,12 @@
 import { instance } from '@/api/instance';
+import { LogOut, Login } from '@/types/auth';
 
-export async function guestLogin() {
+export async function guestLogin(): Promise<Login> {
 	const response = await instance.post('/auth/guest-login');
 	return response.data;
 }
 
-export async function logout(token: string) {
+export async function logout(token: string): Promise<LogOut> {
 	const response = await instance.delete('/auth/logout', {
 		headers: {
 			Authorization: `Bearer ${token}`,
