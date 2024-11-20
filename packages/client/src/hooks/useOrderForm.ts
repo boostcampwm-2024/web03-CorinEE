cc4dd1015415db4a5adbe8265c7c769bdb3ff15f
qwer0114 <skyok203@naver.com>
2024-11-20 12:37:10 +0900

    refactor: 매수/매도 상태들 커스텀 훅으로 분리

---
+++ packages/client/src/hooks/useOrderForm.ts
@@ -0,0 +1,64 @@
+import { useState, useEffect } from 'react';
+import { useTrade } from '@/hooks/useTrade';
+import { Market } from '@/types/market';
+import { FormEvent } from 'react';
+import { useMarketParams } from '@/hooks/market/useMarketParams';
+type UserOrderForm = {
+	currentPrice: number;
+	askType: 'bid' | 'ask';
+};
+
+export function useOrderForm({ currentPrice, askType }: UserOrderForm) {
+	const [price, setPrice] = useState(String(currentPrice));
+	const [quantity, setQuantity] = useState<string>('');
+	const [quantityErrorMessage, setQuantityErrorMessage] = useState<string>('');
+
+	const tradeMutation = useTrade(askType);
+	const { marketType, code } = useMarketParams();
+
+	const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		if (quantity === '' || !quantity || !Number(quantity)) {
+			setQuantityErrorMessage('수량을 입력해주세요');
+			return;
+		}
+
+		const tradeData =
+			askType === 'bid'
+				? {
+						askType: 'bid' as const,
+						typeGiven: marketType as Market,
+						typeReceived: code,
+						receivedPrice: Number(price),
+						receivedAmount: Number(quantity),
+					}
+				: {
+						askType: 'ask' as const,
+						typeGiven: code,
+						typeReceived: marketType as Market,
+						receivedPrice: Number(price),
+						receivedAmount: Number(quantity),
+					};
+
+		tradeMutation.mutate(tradeData);
+	};
+
+	useEffect(() => {
+		if (quantityErrorMessage) {
+			const timer = setTimeout(() => {
+				setQuantityErrorMessage('');
+			}, 1500);
+
+			return () => clearTimeout(timer);
+		}
+	}, [quantityErrorMessage]);
+
+	return {
+		price,
+		setPrice,
+		quantity,
+		setQuantity,
+		quantityErrorMessage,
+		handleSubmit,
+	};
+}
