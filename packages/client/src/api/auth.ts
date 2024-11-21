159558959181250801134f65986f1a9f8897bfa1
SeungGwa123 <skwan123@naver.com>
2024-11-21 19:55:37 +0900

    fix: be conflict fix

--- packages/client/src/api/auth.ts
+++ packages/client/src/api/auth.ts
@@ -2,12 +2,12 @@ import { instance } from '@/api/instance';
 import { LogOut, Login } from '@/types/auth';
 
 export async function guestLogin(): Promise<Login> {
-	const response = await instance.post('/api/auth/guest-login');
+	const response = await instance.post('/auth/guest-login');
 	return response.data;
 }
 
 export async function logout(token: string): Promise<LogOut> {
-	const response = await instance.delete('/api/auth/logout', {
+	const response = await instance.delete('/auth/logout', {
 		headers: {
 			Authorization: `Bearer ${token}`,
 		},
