9f11d66e4bc0deeb167c6537f9e3af2c5a4677e6
Seonghyeon0409 <t139754@gmail.com>
2024-11-22 21:34:55 +0900

    feat: refresh 토큰 컨트롤러 및 구조 개선

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,9 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {
-  SwaggerModule,
-  DocumentBuilder,
-  SwaggerCustomOptions,
+	SwaggerModule,
+	DocumentBuilder,
+	SwaggerCustomOptions,
 } from '@nestjs/swagger';
 import { config } from 'dotenv';
 import { setupSshTunnel } from './configs/ssh-tunnel';
@@ -12,39 +12,38 @@ import { AllExceptionsFilter } from 'common/all-exceptions.filter';
 config();
 
 async function bootstrap() {
-  await setupSshTunnel();
-  const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  });
+	await setupSshTunnel();
+	const app = await NestFactory.create(AppModule);
+	app.enableCors({
+		origin: true,
+		methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+		credentials: true,
+	});
 
-  const config = new DocumentBuilder()
-    .setTitle('CorinEE API example')
-    .setDescription('CorinEE API description')
-    .setVersion('1.0')
-    .addTag('corinee')
-    .addBearerAuth(
-      {
-        type: 'http',
-        scheme: 'bearer',
-        name: 'Authorization',
-        in: 'header',
-      },
-      'access-token',
-    )
-    .build();
-  const customOptions: SwaggerCustomOptions = {
-    swaggerOptions: {
-      persistAuthorization: true,
-    },
-  };
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
+	const config = new DocumentBuilder()
+		.setTitle('CorinEE API example')
+		.setDescription('CorinEE API description')
+		.setVersion('1.0')
+		.addTag('corinee')
+		.addBearerAuth(
+			{
+				type: 'http',
+				scheme: 'bearer',
+				bearerFormat: 'JWT',
+				description: 'Access token used for authentication',
+			},
+			'access-token',
+		).build();
+	const customOptions: SwaggerCustomOptions = {
+		swaggerOptions: {
+			persistAuthorization: true,
+		},
+	};
+	const documentFactory = () => SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup('api', app, documentFactory);
 
-  app.setGlobalPrefix('api');
-  app.useGlobalFilters(new AllExceptionsFilter());
+	app.setGlobalPrefix('api');
+	app.useGlobalFilters(new AllExceptionsFilter());
 
 	await app.listen(process.env.PORT ?? 3000);
 }
