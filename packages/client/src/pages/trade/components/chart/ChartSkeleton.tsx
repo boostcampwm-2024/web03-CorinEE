404c6549a8f34b434a4ea3a31728e28e40b4b00b
qwer0114 <skyok203@naver.com>
2024-11-22 18:02:20 +0900

    refactor: 로딩 컴포넌트 구현

--- packages/client/src/pages/trade/components/chart/ChartSkeleton.tsx
+++ packages/client/src/pages/trade/components/chart/ChartSkeleton.tsx
@@ -1,6 +1,10 @@
+import Loading from '@/components/Loading';
+
 function ChartSkeleton() {
 	return (
-		<div className="bg-gray-50 min-w-80 rounded-lg flex-[2] overflow-hidden"></div>
+		<div className="bg-gray-50 min-w-80 rounded-lg flex-[2] overflow-hidden">
+			<Loading />
+		</div>
 	);
 }
 
