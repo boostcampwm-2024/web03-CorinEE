e776c1dfa3ce9518e1c60863fd74d961d020dde6
Seonghyeon0409 <t139754@gmail.com>
2024-11-23 00:11:08 +0900

    feat: google oauth구현

--- packages/server/src/auth/auth.module.ts
+++ packages/server/src/auth/auth.module.ts
@@ -8,18 +8,29 @@ import { AuthService } from './auth.service';
 import { AccountRepository } from 'src/account/account.repository';
 import { AuthController } from './auth.controller';
 import { AccountModule } from 'src/account/account.module';
+import { KakaoStrategy } from './strategies/kakao.strategy';
+import { GoogleStrategy } from './strategies/google.strategy';
+import { PassportModule } from '@nestjs/passport';
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([User]),
-    JwtModule.register({
-      global: true,
-      secret: jwtConstants.secret,
-      signOptions: { expiresIn: '6000s' },
-    }),
-    AccountModule,
-  ],
-  providers: [UserRepository, AccountRepository, AuthService, JwtService],
-  controllers: [AuthController],
-  exports: [UserRepository],
+	imports: [
+		TypeOrmModule.forFeature([User]),
+		JwtModule.register({
+			global: true,
+			secret: jwtConstants.secret,
+			signOptions: { expiresIn: '6000s' },
+		}),
+		AccountModule,
+    PassportModule
+	],
+	providers: [
+		UserRepository,
+		AccountRepository,
+		AuthService,
+		JwtService,
+		GoogleStrategy,
+		KakaoStrategy,
+	],
+	controllers: [AuthController],
+	exports: [UserRepository],
 })
 export class AuthModule {}
