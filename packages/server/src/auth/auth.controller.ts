c498a356659d139b6917e14b79d641e6ce537f7f
Seonghyeon0409 <t139754@gmail.com>
2024-11-22 20:49:39 +0900

    feat: auth refresh token 구현

--- packages/server/src/auth/auth.controller.ts
+++ packages/server/src/auth/auth.controller.ts
@@ -1,69 +1,77 @@
 import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  HttpCode,
-  HttpStatus,
-  Post,
-  Request,
-  UseGuards,
+	Body,
+	Controller,
+	Delete,
+	Get,
+	HttpCode,
+	HttpStatus,
+	Post,
+	Request,
+	UseGuards,
 } from '@nestjs/common';
 import { AuthGuard } from './auth.guard';
 import { AuthService } from './auth.service';
 import {
-  ApiBody,
-  ApiBearerAuth,
-  ApiSecurity,
-  ApiResponse,
+	ApiBody,
+	ApiBearerAuth,
+	ApiSecurity,
+	ApiResponse,
 } from '@nestjs/swagger';
 import { SignInDto } from './dtos/sign-in.dto';
 import { SignUpDto } from './dtos/sign-up.dto';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private authService: AuthService) {}
+	constructor(private authService: AuthService) {}
 
-  @ApiBody({ type: SignInDto })
-  @HttpCode(HttpStatus.OK)
-  @Post('login')
-  signIn(@Body() signInDto: Record<string, any>) {
-    return this.authService.signIn(signInDto.username);
-  }
+	@ApiBody({ type: SignInDto })
+	@HttpCode(HttpStatus.OK)
+	@Post('login')
+	signIn(@Body() signInDto: Record<string, any>) {
+		return this.authService.signIn(signInDto.username);
+	}
 
-  @HttpCode(HttpStatus.OK)
-  @Post('guest-login')
-  guestSignIn() {
-    return this.authService.guestSignIn();
-  }
+	@HttpCode(HttpStatus.OK)
+	@Post('guest-login')
+	guestSignIn() {
+		return this.authService.guestSignIn();
+	}
 
-  @ApiResponse({
-    status: HttpStatus.OK,
-    description: 'New user successfully registered',
-  })
-  @ApiResponse({
-    status: HttpStatus.BAD_REQUEST,
-    description: 'Invalid input or user already exists',
-  })
-  @HttpCode(HttpStatus.CREATED)
-  @Post('signup')
-  async signUp(@Body() signUpDto: SignUpDto) {
-    return this.authService.signUp(signUpDto.username);
-  }
+	@ApiResponse({
+		status: HttpStatus.OK,
+		description: 'New user successfully registered',
+	})
+	@ApiResponse({
+		status: HttpStatus.BAD_REQUEST,
+		description: 'Invalid input or user already exists',
+	})
+	@HttpCode(HttpStatus.CREATED)
+	@Post('signup')
+	async signUp(@Body() signUpDto: SignUpDto) {
+		return this.authService.signUp(signUpDto.username);
+	}
 
-  @ApiBearerAuth('access-token')
-  @ApiSecurity('access-token')
-  @UseGuards(AuthGuard)
-  @Delete('logout')
-  logout(@Request() req) {
-    return this.authService.logout(req.user.userId);
-  }
+	@ApiBearerAuth('access-token')
+	@ApiSecurity('access-token')
+	@UseGuards(AuthGuard)
+	@Delete('logout')
+	logout(@Request() req) {
+		return this.authService.logout(req.user.userId);
+	}
 
-  @UseGuards(AuthGuard)
-  @ApiBearerAuth('access-token')
-  @ApiSecurity('access-token')
-  @Get('profile')
-  getProfile(@Request() req) {
-    return req.user;
-  }
+	@UseGuards(AuthGuard)
+	@ApiBearerAuth('access-token')
+	@ApiSecurity('access-token')
+	@Get('profile')
+	getProfile(@Request() req) {
+		return req.user;
+	}
+
+	@ApiBearerAuth('refresh-token')
+	@ApiSecurity('refresh-token')
+	@HttpCode(HttpStatus.OK)
+	@Post('refresh')
+	refreshTokens(@Body() body: { userId: number; refreshToken: string }) {
+		return this.authService.refreshTokens(body.userId, body.refreshToken);
+	}
 }
