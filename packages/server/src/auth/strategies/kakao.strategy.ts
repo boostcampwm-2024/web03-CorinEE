e63fd037471595510a99b095333833102d57795f
Seonghyeon0409 <t139754@gmail.com>
2024-11-23 00:11:08 +0900

    feat: google oauth구현

--- packages/server/src/auth/strategies/kakao.strategy.ts
+++ packages/server/src/auth/strategies/kakao.strategy.ts
@@ -4,29 +4,29 @@ import { Strategy, Profile } from 'passport-kakao';
 
 @Injectable()
 export class KakaoStrategy extends PassportStrategy(Strategy, 'kakao') {
-  constructor() {
-    super({
-      clientID: process.env.KAKAO_CLIENT_ID,
-      clientSecret: '',
-      callbackURL: `${process.env.CALLBACK_URL}/api/auth/kakao/callback`
-    });
-  }
+	constructor() {
+		super({
+			clientID: process.env.KAKAO_CLIENT_ID,
+			clientSecret: '',
+			callbackURL: `${process.env.CALLBACK_URL}/api/auth/kakao/callback`,
+		});
+	}
 
-  async validate(
-    accessToken: string,
-    refreshToken: string,
-    profile: Profile,
-    done: Function,
-  ): Promise<any> {
-    const { id, username, _json } = profile;
-    const user = {
-      provider: 'kakao',
-      id,
-      name: username,
-      email: _json.kakao_account?.email,
-      accessToken,
-      refreshToken,
-    };
-    done(null, user);
-  }
+	async validate(
+		accessToken: string,
+		refreshToken: string,
+		profile: Profile,
+		done: Function,
+	): Promise<any> {
+		const { id, username, _json } = profile;
+		const user = {
+			provider: 'kakao',
+			id,
+			name: username,
+			email: _json.kakao_account?.email,
+			accessToken,
+			refreshToken,
+		};
+		done(null, user);
+	}
 }
