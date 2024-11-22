d3acf70fa834f2946e9b7c5b2acb71e9b27b3edc
SeungGwa123 <skwan123@naver.com>
2024-11-22 14:05:39 +0900

    fix: 퍼센트 api 로직 에러 수정

--- packages/server/src/trade/trade-bid.service.ts
+++ packages/server/src/trade/trade-bid.service.ts
@@ -191,7 +191,6 @@ export class BidService implements OnModuleInit {
 			}
 
 			tradeData.quantity -= parseFloat(buyData.quantity.toFixed(8));
-
 			if (tradeData.quantity <= 0.00000001) {
 				await this.tradeRepository.deleteTrade(tradeId, queryRunner);
 			} else
