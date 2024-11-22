1714cae51a3471d2f8b0c6cbf76d9c42f6d823c8
qwer0114 <skyok203@naver.com>
2024-11-22 18:02:10 +0900

    refactor: 부분 로딩으로 변경

--- packages/client/src/pages/trade/Trade.tsx
+++ packages/client/src/pages/trade/Trade.tsx
@@ -5,32 +5,30 @@ import TradeHeader from '@/pages/trade/components/trade_header/TradeHeader';
 import { useParams } from 'react-router-dom';
 import { useSSETicker } from '@/hooks/SSE/useSSETicker';
 import { Suspense, useMemo, useState } from 'react';
-import { useSSEOrderbook } from '@/hooks/SSE/useSSEOrderbook';
 import ChartSkeleton from '@/pages/trade/components/chart/ChartSkeleton';
 
 function Trade() {
 	const { market } = useParams();
 	const marketCode = useMemo(() => (market ? [{ market }] : []), [market]);
-	const { sseData } = useSSETicker(marketCode);
-	const { sseData: orderBook } = useSSEOrderbook(marketCode);
+	const { sseData: price } = useSSETicker(marketCode);
 	const [selectPrice, setSelectPrice] = useState<number | null>(null);
 
-	if (!market || !sseData || !orderBook) return;
+	if (!market || !price) return;
 
-	const currentPrice = sseData[market]?.trade_price;
+	const currentPrice = price[market]?.trade_price;
 	const handleSelectPrice = (price: number) => {
 		setSelectPrice(price);
 	};
 	return (
 		<div className="w-full gap-2">
-			<TradeHeader market={market} sseData={sseData} />
+			<TradeHeader market={market} sseData={price} />
 			<div className="flex gap-2 max-h-[75vh] overflow-y-hidden">
 				<Suspense fallback={<ChartSkeleton />}>
 					<Chart market={market} />
 				</Suspense>
 				<OrderBook
-					orderBook={orderBook[market]}
 					currentPrice={currentPrice}
+					marketCode={marketCode}
 					handleSelectPrice={handleSelectPrice}
 				/>
 				<OrderForm currentPrice={currentPrice} selectPrice={selectPrice} />
