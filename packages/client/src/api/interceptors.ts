8684c97283b4c6864e9b947a1e64cc357828cde1
qwer0114 <skyok203@naver.com>
2024-11-24 17:52:39 +0900

    refactor: http only 옵션 제거

--- packages/client/src/api/interceptors.ts
+++ packages/client/src/api/interceptors.ts
@@ -1,6 +1,6 @@
 import { authInstance, instance } from '@/api/instance';
 import { Login } from '@/types/auth';
-import { getCookie, setCookie } from '@/utility/cookies';
+import { getCookie, removeCookie, setCookie } from '@/utility/cookies';
 import { AxiosResponse } from 'axios';
 
 authInstance.interceptors.request.use(
@@ -20,7 +20,7 @@ authInstance.interceptors.response.use(
 	(response) => response,
 	async (error) => {
 		const originalRequest = error.config;
-		if (error.response.status === 401 && !originalRequest._retry) {
+		if (error.response.status === 401) {
 			try {
 				const response: AxiosResponse<Login> = await instance.post(
 					'/auth/refresh',
@@ -34,12 +34,12 @@ authInstance.interceptors.response.use(
 				localStorage.setItem('access_token', new_access_token);
 				setCookie('refresh_token', new_refresh_token, {
 					path: '/',
-					httpOnly: true,
 				});
 				originalRequest.headers.Authorization = `Bearer ${new_access_token}`;
 				return authInstance(originalRequest);
 			} catch (error) {
 				localStorage.removeItem('access_token');
+				removeCookie('refresh_token');
 				window.location.href = '/';
 				return Promise.reject(error);
 			}
