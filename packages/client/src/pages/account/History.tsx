deff3120bd9b3cc9918155051eaa36e3a61cbc2e
Jun Hee Jung <wjdtka1325@naver.com>
2024-11-24 21:28:25 +0900

    feat: 거래내역 api 연결 구현

--- packages/client/src/pages/account/History.tsx
+++ packages/client/src/pages/account/History.tsx
@@ -1,4 +1,11 @@
+import { useMyHistory } from '@/hooks/useMyHistory';
+
+
 function History() {
+	const histories = useMyHistory()
+
+	console.log(histories)
+
 	return <div>History</div>;
 }
 
---
+++ packages/client/src/types/history.ts
@@ -0,0 +1,10 @@
+export type History = {
+	assetName: string;
+	createdAt: string;
+	price: number;
+	quantity: number;
+	tradeCurrency: string;
+	tradeDate: string;
+	tradeHistoryId: number;
+	tradeType: string;
+};
