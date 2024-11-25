91033612b7fd67f19ec199bb17aaef2f7dca4400
qwer0114 <skyok203@naver.com>
2024-11-25 21:39:36 +0900

    feat: 차트 현재가 분봉 등락 실시간 기능 정의

--- packages/client/src/pages/trade/components/chart/Chart.tsx
+++ packages/client/src/pages/trade/components/chart/Chart.tsx
@@ -4,7 +4,12 @@ import ChartSelector from '@/pages/trade/components/chart/ChartSelector';
 import { CandlePeriod } from '@/types/chart';
 import CandleChart from '@/pages/trade/components/chart/CandleChart';
 
-function Chart({ market }: { market: string }) {
+type ChartProps = {
+	market: string;
+	currentPrice: number;
+};
+
+function Chart({ market, currentPrice }: ChartProps) {
 	const [activePeriod, setActivePeriod] = useState<CandlePeriod>('days');
 	const [minute, setMinute] = useState<number>();
 	const { data, fetchNextPage } = usePeriodChart(market, activePeriod, minute);
@@ -15,7 +20,7 @@ function Chart({ market }: { market: string }) {
 	};
 
 	return (
-		<div className="bg-gray-50 min-w-80 rounded-lg flex-[2] overflow-hidden">
+		<div className="bg-gray-50 min-w-80 rounded-lg flex-[2] overflow-y-auto overflow-x-hidden min-h-0 z-0">
 			<ChartSelector
 				activePeriods={activePeriod}
 				handleActivePeriod={handleActivePeriod}
@@ -25,6 +30,7 @@ function Chart({ market }: { market: string }) {
 				minute={minute}
 				data={data.candles.flat()}
 				fetchNextPage={fetchNextPage}
+				currentPrice={currentPrice}
 			/>
 		</div>
 	);
