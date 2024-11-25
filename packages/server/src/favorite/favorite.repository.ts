ce95b2795a7278955dc2e6fefcb3ee747c748743
Seonghyeon0409 <t139754@gmail.com>
2024-11-25 17:33:57 +0900

    feat: favorite module 및 api 구현

---
+++ packages/server/src/favorite/favorite.repository.ts
@@ -0,0 +1,13 @@
+import { DataSource, Repository } from 'typeorm';
+import { Favorite } from './favorite.entity';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class FavoriteRepository extends Repository<Favorite> {
+  constructor(
+    private dataSource: DataSource,
+  ) {
+    super(Favorite, dataSource.createEntityManager());
+  }
+
+}
---
+++ packages/server/src/favorite/favorite.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@nestjs/common';
+import { FavoriteRepository } from './favorite.repository';
+
+@Injectable()
+export class FavoriteService {
+	constructor(private favoriteRepository: FavoriteRepository) {}
+
+	async getFavorites(user, assetName) {
+		if (assetName) {
+            const result = await this.favoriteRepository.find({
+                where: {
+                    user: { id: user.userId },
+                    assetName,
+                },
+            })
+			return {
+				statusCode: 200,
+				result
+			};
+		} else {
+            const result = await this.favoriteRepository.find({
+                where: { user: { id: user.userId } },
+            })
+            console.log(result)
+			return {
+				statusCode: 200,
+				result
+			};
+		}
+	}
+
+	async createFavorite(user, assetName) {
+		return await this.favoriteRepository.save({
+			user: { id: user.userId },
+			assetName,
+		});
+	}
+
+	async deleteFavorite(user, assetName) {
+		return await this.favoriteRepository.delete({
+			user: { id: user.userId },
+			assetName,
+		});
+	}
+
+	async toggleFavorite(user, assetName) {
+		const favorite = await this.favoriteRepository.find({
+			where: {
+				user: { id: user.userId },
+				assetName,
+			},
+		});
+    console.log(favorite);
+		if (favorite.length > 0) {
+			return await this.deleteFavorite(user, assetName);
+		} else {
+			return await this.createFavorite(user, assetName);
+		}
+	}
+}
