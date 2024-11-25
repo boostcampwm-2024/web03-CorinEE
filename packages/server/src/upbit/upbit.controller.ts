62321d629cbae333642ccd1b4dd43de2815e4a97
Seonghyeon0409 <t139754@gmail.com>
2024-11-25 13:58:47 +0900

    feat: 코인 현재가 rest api 구현

--- packages/server/src/upbit/upbit.controller.ts
+++ packages/server/src/upbit/upbit.controller.ts
@@ -8,87 +8,96 @@ import { ApiQuery } from '@nestjs/swagger';
 
 @Controller('upbit')
 export class UpbitController {
-  constructor(
-    private readonly sseService: SseService,
-    private readonly coinListService: CoinListService,
-    private readonly chartService : ChartService
-  ) {}
+	constructor(
+		private readonly sseService: SseService,
+		private readonly coinListService: CoinListService,
+		private readonly chartService: ChartService,
+	) {}
 
-  @Sse('price-updates')
-  priceUpdates(@Query('coins') coins: string[]): Observable<MessageEvent> {
-    coins = coins || [];
-    const initData = this.sseService.initPriceStream(
-      coins,
-      this.coinListService.convertToMarketCoinDto,
-    );
-    return concat(
-      initData,
-      this.sseService.getPriceUpdatesStream(
-        coins,
-        this.coinListService.convertToCodeCoinDto,
-      ),
-    );
-  }
-  @Sse('orderbook')
-  orderbookUpdates(@Query('coins') coins: string[]): Observable<MessageEvent> {
-    coins = coins || [];
-    return this.sseService.getOrderbookUpdatesStream(
-      coins,
-      this.coinListService.convertToOrderbookDto,
-    );
-  }
+	@Sse('price-updates')
+	priceUpdates(@Query('coins') coins: string[]): Observable<MessageEvent> {
+		coins = coins || [];
+		const initData = this.sseService.initPriceStream(
+			coins,
+			this.coinListService.convertToMarketCoinDto,
+		);
+		return concat(
+			initData,
+			this.sseService.getPriceUpdatesStream(
+				coins,
+				this.coinListService.convertToCodeCoinDto,
+			),
+		);
+	}
+	@Sse('orderbook')
+	orderbookUpdates(@Query('coins') coins: string[]): Observable<MessageEvent> {
+		coins = coins || [];
+		return this.sseService.getOrderbookUpdatesStream(
+			coins,
+			this.coinListService.convertToOrderbookDto,
+		);
+	}
 
-  @Get('market/all')
-  getAllMarkets() {
-    return this.coinListService.getAllCoinList();
-  }
-  @Get('market/krw')
-  getKRWMarkets() {
-    return this.coinListService.getKRWCoinList();
-  }
-  @Get('market/btc')
-  getBTCMarkets() {
-    return this.coinListService.getBTCCoinList();
-  }
-  @Get('market/usdt')
-  getUSDTMarkets() {
-    return this.coinListService.getUSDTCoinList();
-  }
+	@Get('market/all')
+	getAllMarkets() {
+		return this.coinListService.getAllCoinList();
+	}
+	@Get('market/krw')
+	getKRWMarkets() {
+		return this.coinListService.getKRWCoinList();
+	}
+	@Get('market/btc')
+	getBTCMarkets() {
+		return this.coinListService.getBTCCoinList();
+	}
+	@Get('market/usdt')
+	getUSDTMarkets() {
+		return this.coinListService.getUSDTCoinList();
+	}
 
-  @Get('market/top20-trade/krw')
-  getTop20TradeKRW() {
-    return this.coinListService.getMostTradeCoin();
-  }
-  @Get('market/simplelist/krw')
-  getSomeKRW(@Query('market') market: string[]) {
-    const marketList = market || [];
-    return this.coinListService.getSimpleCoin(marketList);
-  }
+	@Get('market/top20-trade/krw')
+	getTop20TradeKRW() {
+		return this.coinListService.getMostTradeCoin();
+	}
+	@Get('market/simplelist/krw')
+	getSomeKRW(@Query('market') market: string[]) {
+		const marketList = market || [];
+		return this.coinListService.getSimpleCoin(marketList);
+	}
+	@Get('market/tickers')
+	@ApiQuery({ name: 'coins', required: false, type: String })
+	getCoinTickers(@Query('coins') coins?: string) {
+		return this.coinListService.getCoinTickers(coins);
+	}
 
-  @Get('candle/:type/:minute?')
-  @ApiQuery({ name: 'minute', required: false, type: String })
-  async getCandle(
-    @Res() res: Response,
-    @Param('type') type : string,
-    @Query('market') market: string, 
-    @Query('to') to:string,
-    @Param('minute') minute? :string
-  ){
-    try{
-      console.log("type : "+type)
-      console.log("market : "+market)
-      console.log("minute : "+minute)
-      console.log("to : "+to)
-      const response = await this.chartService.upbitApiDoor(type, market, to, minute)
+	@Get('candle/:type/:minute?')
+	@ApiQuery({ name: 'minute', required: false, type: String })
+	async getCandle(
+		@Res() res: Response,
+		@Param('type') type: string,
+		@Query('market') market: string,
+		@Query('to') to: string,
+		@Param('minute') minute?: string,
+	) {
+		try {
+			console.log('type : ' + type);
+			console.log('market : ' + market);
+			console.log('minute : ' + minute);
+			console.log('to : ' + to);
+			const response = await this.chartService.upbitApiDoor(
+				type,
+				market,
+				to,
+				minute,
+			);
 
-      return res.status(response.statusCode).json(response)
-    }catch(error){
-      console.error("error"+error)
-      return res.status(error.status)
-        .json({
-          message: error.message || '서버오류입니다.',
-          error: error?.response || null,
-        });;
-    }
-  }
+			return res.status(response.statusCode).json(response);
+		} catch (error) {
+			console.error('error' + error);
+			return res.status(error.status).json({
+				message: error.message || '서버오류입니다.',
+				error: error?.response || null,
+			});
+		}
+	}
 }
