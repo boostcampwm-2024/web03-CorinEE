80b74f6f7be9891ea4594f0ba091149626633f6c
qwer0114 <skyok203@naver.com>
2024-11-25 14:44:38 +0900

    refactor: 컴포넌트 분리

--- packages/client/src/pages/trade/components/trade_footer/TradeFooter.tsx
+++ packages/client/src/pages/trade/components/trade_footer/TradeFooter.tsx
@@ -1,39 +1,23 @@
+import { useMarketTicker } from '@/hooks/market/useMarketTicker';
 import { useMarketTop20 } from '@/hooks/market/useMarketTop20';
-import { useNavigate } from 'react-router-dom';
-import { QueryClient } from '@tanstack/react-query';
-import useRecentlyMarketStore from '@/store/recentlyViewed';
+import TradeFooterCoin from '@/pages/trade/components/trade_footer/TradeFooterCoin';
 
 function TradeFooter() {
-	const queryClient = new QueryClient();
 	const { data: coins } = useMarketTop20();
-	const { addRecentlyViewedMarket } = useRecentlyMarketStore();
-	const navigate = useNavigate();
+	const { data: ticker } = useMarketTicker(coins ?? []);
+
 	const duplicatedCoins = coins ? [...coins, ...coins, ...coins, ...coins] : [];
 
-	const handleClick = (market: string) => {
-		addRecentlyViewedMarket(market);
-		navigate(`/trade/${market}`);
-		queryClient.invalidateQueries({ queryKey: ['recentlyMarketList'] });
-	};
 	return (
-		<div className="absolute bottom-0 w-full bg-white overflow-hidden">
+		<div className="absolute bottom-1 w-full bg-white">
 			<div className="inline-flex relative">
 				<div className="flex items-center whitespace-nowrap animate-infiniteScroll">
 					{duplicatedCoins?.map((coin, index) => (
-						<div
-							onClick={() => handleClick(coin.market)}
+						<TradeFooterCoin
 							key={`${coin.market}-${index}`}
-							className="flex items-center shrink-0 p-2 mr-4 cursor-pointer rounded-md hover:bg-gray-200"
-						>
-							<img
-								className="w-6 h-6 mr-2"
-								src={coin.image_url}
-								alt={coin.korean_name}
-							/>
-							<span className="whitespace-nowrap text-sm">
-								{coin.korean_name}
-							</span>
-						</div>
+							coin={coin}
+							ticker={ticker}
+						/>
 					))}
 				</div>
 			</div>
