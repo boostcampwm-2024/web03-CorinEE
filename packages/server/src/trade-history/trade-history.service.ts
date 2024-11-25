b6bcbef4c150f16462b351af703bdac53a26fea3
Seonghyeon0409 <t139754@gmail.com>
2024-11-25 16:09:31 +0900

    fix: tradehistory api 스펙 변경
    Co-authored-by: 이승관 <SeungGwan123@users.noreply.github.com>

--- packages/server/src/trade-history/trade-history.service.ts
+++ packages/server/src/trade-history/trade-history.service.ts
@@ -1,20 +1,14 @@
 import {
 	Injectable,
-	OnModuleInit,
-	UnprocessableEntityException,
 } from '@nestjs/common';
 import { DataSource } from 'typeorm';
-import { CoinDataUpdaterService } from 'src/upbit/coin-data-updater.service';
 import { TradeHistoryRepository } from '../trade-history/trade-history.repository';
-import { UPBIT_IMAGE_URL } from 'common/upbit';
-import { TradeHistoryDataDto } from './dtos/tradehistoryData.dto';
 
 @Injectable()
 export class TradeHistoryService {
 
 	constructor(
         private tradehistoryRepository: TradeHistoryRepository,
-        private coinDataUpdaterService: CoinDataUpdaterService,
 		private readonly dataSource: DataSource,
 	) {}
 
@@ -32,31 +26,12 @@ export class TradeHistoryService {
                     result : []
                 }
             }
-            const coinNameData = await this.coinDataUpdaterService.getCoinNameList();
 
             if(coin){
                 const [assetName, tradeCurrency] = coin.split("-")
                 tradehistoryData = tradehistoryData.filter(({ assetName: a, tradeCurrency: t }) => (a === assetName && t === tradeCurrency) || (a === tradeCurrency && t === assetName));
             }
-            tradehistoryData.forEach(tradehistory=>{
-                const name = tradehistory.tradeType === 'buy' ? tradehistory.tradeCurrency : tradehistory.assetName;
-                const tradeType = tradehistory.tradeType
-                const tradedata: TradeHistoryDataDto = {
-                    img_url : `${UPBIT_IMAGE_URL}${name}.png`,
-                    koreanName : coinNameData.get(`${tradehistory.assetName}-${tradehistory.tradeCurrency}`) || coinNameData.get(`${tradehistory.tradeCurrency}-${tradehistory.assetName}`),
-                    coin : tradeType === 'buy' ? tradehistory.assetName : tradehistory.tradeCurrency,
-                    market : tradeType === 'sell' ? tradehistory.assetName : tradehistory.tradeCurrency,
-                    tradeType : tradeType,
-                    price : tradehistory.price,
-                    averagePrice : tradehistory.quantity / tradehistory.price,
-                    quantity : tradehistory.quantity,
-                    createdAt : tradehistory.createdAt,
-                    tradeDate : tradehistory.tradeDate,
-                    userId : user.userId
-                };
 
-                result.push(tradedata)
-            })
             return {
                 statusCode : 200,
                 message : "거래 내역을 찾았습니다.",
