3a7edadcfc88b356a1d12a08a33156f452e350b9
qwer0114 <skyok203@naver.com>
2024-11-26 23:16:24 +0900

    refactor: 불필요한 fragment 제거

--- packages/client/src/components/Header.tsx
+++ packages/client/src/components/Header.tsx
@@ -1,6 +1,6 @@
 import { useAuth } from '@/hooks/auth/useAuth';
 import { Button, Navbar } from '@material-tailwind/react';
-import { Link, NavLink, useLocation, useMatch } from 'react-router-dom';
+import { Link, NavLink, useLocation } from 'react-router-dom';
 import { useAuthStore } from '@/store/authStore';
 import { useToast } from '@/hooks/ui/useToast';
 import logoImage from '@asset/logo/corineeLogo.png';
@@ -22,65 +22,63 @@ function Header() {
 	const isAccountPage = location.pathname.startsWith('/account');
 
 	return (
-		<>
-			<Navbar
-				fullWidth={true}
-				className="w-full flex justify-between items-center sticky top-0 z-10"
-				shadow={false}
-			>
-				<div>
-					<Link to={'/'} className="flex gap-2 items-center">
-						<img className="w-12 h-12" src={logoImage} />
-						<h1 className="text-black text-xl font-semibold">Corinee</h1>
-					</Link>
-				</div>
-				<div className="flex gap-4">
-					<NavLink
-						to={'/'}
-						className={({ isActive }) =>
-							`${isActive ? 'text-black font-semibold' : 'text-gray-600'} hover:text-black`
-						}
+		<Navbar
+			fullWidth={true}
+			className="w-full flex justify-between items-center sticky top-0 z-10"
+			shadow={false}
+		>
+			<div>
+				<Link to={'/'} className="flex gap-2 items-center">
+					<img className="w-12 h-12" src={logoImage} />
+					<h1 className="text-black text-xl font-semibold">Corinee</h1>
+				</Link>
+			</div>
+			<div className="flex gap-4">
+				<NavLink
+					to={'/'}
+					className={({ isActive }) =>
+						`${isActive ? 'text-black font-semibold' : 'text-gray-600'} hover:text-black`
+					}
+				>
+					홈
+				</NavLink>
+				<NavLink
+					to={'/account/balance'}
+					className={`${isAccountPage ? 'text-black font-semibold' : 'text-gray-600'} hover:text-black`}
+				>
+					내 계좌
+				</NavLink>
+			</div>
+			<div>
+				{isAuthenticated ? (
+					<Button
+						onClick={() => {
+							logout.mutateAsync();
+							toast.success('로그아웃됐어요');
+						}}
 					>
-						홈
-					</NavLink>
-					<NavLink
-						to={'/account/balance'}
-						className={`${isAccountPage ? 'text-black font-semibold' : 'text-gray-600'} hover:text-black`}
-					>
-						내 계좌
-					</NavLink>
-				</div>
-				<div>
-					{isAuthenticated ? (
+						로그아웃
+					</Button>
+				) : (
+					<div className="flex gap-3">
 						<Button
 							onClick={() => {
-								logout.mutateAsync();
-								toast.success('로그아웃됐어요');
+								guestLogin.mutateAsync();
+								toast.success('안녕하세요');
 							}}
 						>
-							로그아웃
+							체험하기
 						</Button>
-					) : (
-						<div className="flex gap-3">
-							<Button
-								onClick={() => {
-									guestLogin.mutateAsync();
-									toast.success('안녕하세요');
-								}}
-							>
-								체험하기
-							</Button>
-							<button onClick={() => handleLogin('kakao')}>
-								<img alt="kakao_image" src={kakaLogo} className="w-10 h-10" />
-							</button>
-							<button onClick={() => handleLogin('google')}>
-								<img alt="google_image" src={googleLogo} className="w-9 h-9" />
-							</button>
-						</div>
-					)}
-				</div>
-			</Navbar>
-		</>
+						<button onClick={() => handleLogin('kakao')}>
+							<img alt="kakao_image" src={kakaLogo} className="w-10 h-10" />
+						</button>
+						<button onClick={() => handleLogin('google')}>
+							<img alt="google_image" src={googleLogo} className="w-9 h-9" />
+						</button>
+					</div>
+				)}
+			</div>
+		</Navbar>
 	);
 }
 
