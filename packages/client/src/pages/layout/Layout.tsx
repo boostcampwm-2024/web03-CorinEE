7025a5d042e96b27fede91b37fa949c3ecf6d129
qwer0114 <skyok203@naver.com>
2024-11-26 23:17:38 +0900

    refactor: css 수정

--- packages/client/src/pages/layout/Layout.tsx
+++ packages/client/src/pages/layout/Layout.tsx
@@ -4,16 +4,15 @@ import { Outlet } from 'react-router-dom';
 
 function Layout() {
 	return (
-		<div className="flex min-h-screen h-screen overflow-x-auto overflow-y-auto">
+		<div className="flex min-h-screen h-screen overflow-x-hidden overflow-y-auto">
 			<div className="w-full flex flex-col">
 				<Header />
 				<main className="flex-1">
-					<div className="pt-10 px-20">
+					<div className="h-full pt-5 px-20">
 						<Outlet />
 					</div>
 				</main>
 			</div>
-
 			<div className="sticky top-0 right-0 h-screen shrink-0 overflow-visible z-20">
 				<Sidebar />
 			</div>
