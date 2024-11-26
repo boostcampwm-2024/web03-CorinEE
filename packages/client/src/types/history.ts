1648e16318028460a7a219103c98346b2b0b15fc
Jun Hee Jung <wjdtka1325@naver.com>
2024-11-26 10:17:06 +0900

    feat: 내 계좌 거래내역 구현

--- packages/client/src/types/history.ts
+++ packages/client/src/types/history.ts
@@ -1,10 +1,14 @@
-export type History = {
+export type AccountHistory = {
 	assetName: string;
 	createdAt: string;
 	price: number;
 	quantity: number;
 	tradeCurrency: string;
 	tradeDate: string;
-	tradeHistoryId: number;
+	tradeHistoryId?: number;
 	tradeType: string;
 };
+
+export type Period = 'ONE_WEEK' | 'ONE_MONTH' | 'THREE_MONTH' | 'SIX_MONTH' | 'TOTAL'
+
+export type Option = 'TOTAL' | 'BUY' | 'SELL'
