6ecb1244fc6bbec4401dc890d345c6d67b32dd2e
qwer0114 <skyok203@naver.com>
2024-11-27 13:12:11 +0900

    feat: 체험용 로그인 모달 정의

--- packages/client/src/components/Header.tsx
+++ packages/client/src/components/Header.tsx
@@ -2,82 +2,81 @@ import { useAuth } from '@/hooks/auth/useAuth';
 import { Button, Navbar } from '@material-tailwind/react';
 import { Link, NavLink } from 'react-router-dom';
 import { useAuthStore } from '@/store/authStore';
-import { useToast } from '@/hooks/ui/useToast';
 import logoImage from '@asset/logo/corineeLogo.png';
 import kakaLogo from '@asset/logo/kakao.png';
 import googleLogo from '@asset/logo/google.png';
 import { GOOGLE_AUTH_URL, KAKAO_AUTH_URL } from '@/constants/authAddress';
+import { useModal } from '@/hooks/ui/useModal';
+import GuestLoginModal from '@/components/modal/GuestLoginModal';
+import { useToast } from '@/hooks/ui/useToast';
 
 function Header() {
-	const { guestLogin, logout } = useAuth();
-	const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
+	const { logout } = useAuth();
+	const { open, handleOpen } = useModal();
 	const toast = useToast();
+	const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
+	const isGuestLogin = useAuthStore((state) => state.isGuestLogin);
 
 	const handleLogin = (param: 'kakao' | 'google') => {
 		if (param === 'google') window.location.href = GOOGLE_AUTH_URL;
 		else if (param === 'kakao') window.location.href = KAKAO_AUTH_URL;
 	};
 
+	const handleLogout = () => {
+		logout.mutateAsync();
+		if (isGuestLogin) toast.success('체험용 계정이 로그아웃 됐어요');
+		else toast.success('로그아웃에 성공했어요');
+	};
+
 	return (
-		<Navbar
-			fullWidth={true}
-			className="w-full flex justify-between items-center sticky top-0 z-10"
-			shadow={false}
-		>
-			<div>
-				<Link to={'/'} className="flex gap-2 items-center">
-					<img className="w-12 h-12" src={logoImage} />
-					<h1 className="text-black text-xl font-semibold">Corinee</h1>
-				</Link>
-			</div>
-			<div className="flex gap-4">
-				<NavLink
-					to={'/'}
-					className={({ isActive }) =>
-						`${isActive ? 'text-black font-semibold' : 'text-gray-600'} hover:text-black`
-					}
-				>
-					홈
-				</NavLink>
-				<NavLink
-					to={'/account/balance'}
-					className={({ isActive }) =>
-						`${isActive ? 'text-black font-semibold' : 'text-gray-600'} hover:text-black`
-					}
-				>
-					내 계좌
-				</NavLink>
-			</div>
-			<div>
-				{isAuthenticated ? (
-					<Button
-						onClick={() => {
-							logout.mutateAsync();
-							toast.success('로그아웃됐어요');
-						}}
+		<>
+			<Navbar
+				fullWidth={true}
+				className="w-full flex justify-between items-center sticky top-0 z-10"
+				shadow={false}
+			>
+				<div>
+					<Link to={'/'} className="flex gap-2 items-center">
+						<img className="w-12 h-12" src={logoImage} />
+						<h1 className="text-black text-xl font-semibold">Corinee</h1>
+					</Link>
+				</div>
+				<div className="flex gap-4">
+					<NavLink
+						to={'/'}
+						className={({ isActive }) =>
+							`${isActive ? 'text-black font-semibold' : 'text-gray-600'} hover:text-black`
+						}
 					>
-						로그아웃
-					</Button>
-				) : (
-					<div className="flex gap-3">
-						<Button
-							onClick={() => {
-								guestLogin.mutateAsync();
-								toast.success('안녕하세요');
-							}}
-						>
-							체험하기
-						</Button>
-						<button onClick={() => handleLogin('kakao')}>
-							<img alt="kakao_image" src={kakaLogo} className="w-10 h-10" />
-						</button>
-						<button onClick={() => handleLogin('google')}>
-							<img alt="google_image" src={googleLogo} className="w-9 h-9" />
-						</button>
-					</div>
-				)}
-			</div>
-		</Navbar>
+						홈
+					</NavLink>
+					<NavLink
+						to={'/account/balance'}
+						className={({ isActive }) =>
+							`${isActive ? 'text-black font-semibold' : 'text-gray-600'} hover:text-black`
+						}
+					>
+						내 계좌
+					</NavLink>
+				</div>
+				<div className="w-[200px] flex justify-end">
+					{isAuthenticated ? (
+						<Button onClick={handleLogout}>로그아웃</Button>
+					) : (
+						<div className="flex gap-3">
+							<Button onClick={handleOpen}>체험하기</Button>
+							<button onClick={() => handleLogin('kakao')}>
+								<img alt="kakao_image" src={kakaLogo} className="w-10 h-10" />
+							</button>
+							<button onClick={() => handleLogin('google')}>
+								<img alt="google_image" src={googleLogo} className="w-9 h-9" />
+							</button>
+						</div>
+					)}
+				</div>
+			</Navbar>
+			<GuestLoginModal open={open} handleOpen={handleOpen} />
+		</>
 	);
 }
 
