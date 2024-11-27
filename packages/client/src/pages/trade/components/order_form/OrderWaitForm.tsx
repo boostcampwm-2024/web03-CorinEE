99da6eb1020df9730ca5137a6f5dee19b2a875d7
qwer0114 <skyok203@naver.com>
2024-11-27 12:02:58 +0900

    refactor: 대기중인 주문 삭제 기능 정의

--- packages/client/src/pages/trade/components/order_form/OrderWaitForm.tsx
+++ packages/client/src/pages/trade/components/order_form/OrderWaitForm.tsx
@@ -1,25 +1,25 @@
 import { useMarketParams } from '@/hooks/market/useMarketParams';
-import { usePendingCoin } from '@/hooks/trade/usePendingCoin';
-import { useToast } from '@/hooks/ui/useToast';
+import { useDeleteWaitOrder } from '@/hooks/trade/useDeleteWaitOrder';
+import { useMyWaitOrders } from '@/hooks/useMyWaitOrders';
 import NoCoin from '@/pages/trade/components/order_form/common/NoCoin';
 import { PendingCoin } from '@/pages/trade/components/order_form/common/PendingCoin';
 
 function OrderWaitForm() {
 	const { market } = useMarketParams();
-	const { data } = usePendingCoin(market);
-	const toast = useToast();
+	const data = useMyWaitOrders(market);
+	const { deleteOrder } = useDeleteWaitOrder();
 
-	const handleCancel = () => {
-		toast.info('개발중인 기능입니다!');
+	const handleCancel = (tradeId: number, tradeType: string) => {
+		deleteOrder.mutateAsync({ tradeId, tradeType });
 	};
 
-	if (!data.result.length) {
+	if (!data.length) {
 		return <NoCoin message="대기중인 주문이 없어요" />;
 	}
 
 	return (
 		<div className="space-y-2">
-			{data.result.map((order, index) => (
+			{data.map((order, index) => (
 				<PendingCoin key={index} order={order} onCancel={handleCancel} />
 			))}
 		</div>
