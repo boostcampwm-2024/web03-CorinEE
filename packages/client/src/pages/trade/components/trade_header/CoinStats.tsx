6aba53b487fc57fedfd1e7a3393e4c00a4468fde
Jun Hee Jung <wjdtka1325@naver.com>
2024-11-28 03:33:54 +0900

    feat: 상세 페이지에서도 관심 등록 기능 구현

--- packages/client/src/pages/trade/components/trade_header/CoinStats.tsx
+++ packages/client/src/pages/trade/components/trade_header/CoinStats.tsx
@@ -1,15 +1,28 @@
+import { useMyInterest } from '@/hooks/interest/useMyInterest';
+import { useToggleMyInterest } from '@/hooks/interest/useToggleMyInterest';
 import Heart from '@asset/heart.svg?react';
 type CoinStatsProps = {
 	acc_trade_price_24h: string;
 	high_price: string;
 	low_price: string;
+	code: string;
 };
 
 function CoinStats({
 	acc_trade_price_24h,
 	high_price,
 	low_price,
+	code,
 }: CoinStatsProps) {
+	const { toggleInterest } = useToggleMyInterest();
+	const handleToggle = async () => {
+		toggleInterest.mutateAsync(code);
+	};
+	const myInterestMarketList = useMyInterest();
+	const formattedMyInterestMarketList = myInterestMarketList.map(
+		(info) => info.assetName,
+	);
+	const isInterest = formattedMyInterestMarketList.includes(code);
 	const STATS = [
 		{
 			name: '거래대금(24h)',
@@ -39,8 +52,15 @@ function CoinStats({
 					<span className={stat.color}>{stat.value} 원</span>
 				</div>
 			))}
-			<div className="border border-solid border-white rounded-lg p-1 bg-gray-200">
-				<Heart className="w-6 h-6 fill-blue-gray-200 hover:fill-red-400" />
+			<div
+				className="border border-solid border-white rounded-lg p-1 bg-gray-200 cursor-pointer"
+				onClick={handleToggle}
+			>
+				<Heart
+					className={`w-6 h-6 fill-blue-gray-200
+					${isInterest ? 'fill-red-400' : ''}
+					hover:fill-red-400`}
+				/>
 			</div>
 		</div>
 	);
