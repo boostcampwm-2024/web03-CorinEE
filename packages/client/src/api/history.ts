54e6658565fc79001e220a898e4cda2636718987
qwer0114 <skyok203@naver.com>
2024-11-27 11:59:32 +0900

    refactor: 불필요한 중괄호 제거

--- packages/client/src/api/history.ts
+++ packages/client/src/api/history.ts
@@ -2,7 +2,7 @@ import { authInstance } from '@/api/instance';
 import { AccountHistory } from '@/types/history';
 
 export async function myHistory(): Promise<AccountHistory[]> {
-	const response = await authInstance.get(`/tradehistory/tradehistoryData`, {});
+	const response = await authInstance.get(`/tradehistory/tradehistoryData`);
 
 	return response.data.result;
 }
