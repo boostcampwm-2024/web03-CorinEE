ac50fccb12b1bcdb39cf2f5121ccb7236612a64b
qwer0114 <skyok203@naver.com>
2024-11-27 13:16:36 +0900

    refactor: guest login 전역상태 제거

--- packages/client/src/store/authStore.ts
+++ packages/client/src/store/authStore.ts
@@ -2,18 +2,13 @@ import { create } from 'zustand';
 import { getCookie, removeCookie } from '@/utility/storage/cookies';
 interface AuthState {
 	isAuthenticated: boolean;
-	isLoading: boolean;
-	isGuestLogin: boolean;
 	checkAuth: () => void;
 	logout: () => void;
 	getToken: () => string | null;
-	checkGuestLogin: () => void;
 }
 
 export const useAuthStore = create<AuthState>((set) => ({
 	isAuthenticated: !!localStorage.getItem('access_token'),
-	isGuestLogin: false,
-	isLoading: false,
 
 	checkAuth: () => {
 		const hasToken = !!localStorage.getItem('access_token');
@@ -25,8 +20,5 @@ export const useAuthStore = create<AuthState>((set) => ({
 		set({ isAuthenticated: false });
 	},
 
-	checkGuestLogin: () => {
-		set({ isGuestLogin: true });
-	},
 	getToken: () => getCookie('access_token'),
 }));
