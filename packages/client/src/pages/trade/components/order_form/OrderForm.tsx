777fcea71d8083eac8cc3ef3a8a4f75877834e68
qwer0114 <skyok203@naver.com>
2024-11-27 14:33:07 +0900

    refactor: OrderForm 컴포넌트 분리

--- packages/client/src/pages/trade/components/order_form/OrderForm.tsx
+++ packages/client/src/pages/trade/components/order_form/OrderForm.tsx
@@ -1,10 +1,8 @@
-import { useState, Suspense } from 'react';
+import OrderTabs from '@/pages/trade/components/order_form/tabs/OrderTabs';
+import { useState } from 'react';
 import { useAuthStore } from '@/store/authStore';
 import { createOrderTabs } from '@/constants/orderTabs';
-import {
-	ApiErrorBoundary,
-	DefaultErrorFallback,
-} from '@/components/error/ApiErrorBoundary';
+import OrderTabContents from '@/pages/trade/components/order_form/tabs/OrderTabContents';
 
 type OrderFormProps = {
 	currentPrice: number;
@@ -12,45 +10,27 @@ type OrderFormProps = {
 };
 
 function OrderForm({ currentPrice, selectPrice }: OrderFormProps) {
-	const [activeTab, setActiveTab] = useState('buy');
+	const [activeTab, setActiveTab] = useState<'buy' | 'sell' | 'wait'>('buy');
 	const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
 	const TABS = createOrderTabs({ currentPrice, selectPrice });
+	const activeTabData = TABS.find((tab) => tab.id === activeTab);
 
 	return (
 		<div className="bg-gray-50 flex-1 rounded-lg p-2">
 			<div className="text-sm font-semibold">주문하기</div>
 			<div className="flex bg-gray-200 rounded-lg mt-3">
-				{TABS.map((tab) => (
-					<button
-						key={tab.id}
-						onClick={() => setActiveTab(tab.id)}
-						className={`
-							flex-1 text-sm py-2 px-4 rounded-lg transition-colors
-							${activeTab === tab.id ? `${tab.activeColor} bg-white` : 'text-gray-600 hover:bg-gray-100'}
-						`}
-					>
-						{tab.value}
-					</button>
-				))}
+				<OrderTabs
+					tabs={TABS}
+					activeTab={activeTab}
+					onTabChange={setActiveTab}
+				/>
 			</div>
 			<div className="mt-4">
-				{TABS.map(
-					(tab) =>
-						activeTab === tab.id && (
-							<div key={tab.id} className="animate-fadeIn">
-								{isAuthenticated ? (
-									<ApiErrorBoundary
-										fallback={({ error }) => (
-											<DefaultErrorFallback error={error} />
-										)}
-									>
-										<Suspense>{tab.component}</Suspense>
-									</ApiErrorBoundary>
-								) : (
-									tab.notLogin
-								)}
-							</div>
-						),
+				{activeTabData && (
+					<OrderTabContents
+						activeTabData={activeTabData}
+						isAuthenticated={isAuthenticated}
+					/>
 				)}
 			</div>
 		</div>
