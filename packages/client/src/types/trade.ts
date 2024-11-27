c0d6bec310e044d033de0e377683a2be22f6ffc1
qwer0114 <skyok203@naver.com>
2024-11-27 12:03:38 +0900

    refactor: 불필요한 타입 정의 삭제

--- packages/client/src/types/trade.ts
+++ packages/client/src/types/trade.ts
@@ -12,22 +12,3 @@ export type CheckCoin = {
 	statusCode: number;
 	own: boolean;
 };
-
-export type PendingCoinAPI = {
-	statusCode: number;
-	message: 'string';
-	result: PendingCoin[];
-};
-
-export type PendingCoin = {
-	img_url: string;
-	koreanName: string;
-	coin: string;
-	market: string;
-	tradeId: number;
-	tradeType: 'sell' | 'buy';
-	price: number;
-	quantity: number;
-	createdAt: string;
-	userId: number;
-};
