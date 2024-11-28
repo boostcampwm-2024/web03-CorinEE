701179610d95f2aa0a11fe70e62f12adba4db843
SeungGwa123 <skwan123@naver.com>
2024-11-28 16:45:53 +0900

    test

--- packages/server/src/upbit/upbit.controller.ts
+++ packages/server/src/upbit/upbit.controller.ts
@@ -122,20 +122,12 @@ export class UpbitController {
     @Query('coins') coinsQuery?: string[],
   ): Observable<MessageEvent> {
     const coins = this.parseCoins(coinsQuery);
-    
-    const initData$ = from(
-      this.sseService.initOrderStream(
-        coinsQuery,
-        this.coinListService.convertToOrderbookDto,
-      ),
-    )
 
-    const updatesStream = this.sseService.getUpdatesStream(
+    return this.sseService.getUpdatesStream(
       'orderbook',
       coins,
       this.coinListService.convertToOrderbookDto,
     );
-    return concat(initData$, updatesStream)
   }
 
   @ApiOperation({
