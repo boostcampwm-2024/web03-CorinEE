2407efef63fa5eb232cfe9bb05681c1a060cf385
Jun Hee Jung <wjdtka1325@naver.com>
2024-11-28 16:38:14 +0900

    feat: 내 보유, 내 관심 카테고리 삭제 및 한강물 온도 API 연결

--- packages/client/src/pages/home/components/CoinCategories.tsx
+++ packages/client/src/pages/home/components/CoinCategories.tsx
@@ -1,21 +1,22 @@
 import CoinCategory from '@/pages/home/components/CoinCategory';
 import { MarketCategory } from '@/types/category';
+import { TempInfo } from '@/types/tempInfo';
 
 type CoinCategoriesProps = {
 	activeCategory: MarketCategory;
 	handleCategory: (category: MarketCategory) => void;
+	tempInfo: TempInfo | undefined;
 };
 
 export function CoinCategories({
 	activeCategory,
 	handleCategory,
+	tempInfo,
 }: CoinCategoriesProps) {
 	const CATEGORIES: { id: MarketCategory; value: string }[] = [
 		{ id: 'KRW', value: '원화' },
 		{ id: 'BTC', value: 'BTC' },
 		{ id: 'USDT', value: 'USDT' },
-		{ id: 'OWN', value: '보유' },
-		{ id: 'INTEREST', value: '내 관심' },
 	];
 
 	return (
@@ -29,6 +30,17 @@ export function CoinCategories({
 					handleCategory={handleCategory}
 				/>
 			))}
+			{tempInfo && (
+				<div className="flex-[1] text-end">
+					<span className="text-sm text-gray-700">
+						{tempInfo.LAST_UPDATE.split(' ')[0]} 기준 한강물 온도
+					</span>
+					<span className="font-semibold text-xl text-blue-900">
+						{' '}
+						{tempInfo.TEMP}°C
+					</span>
+				</div>
+			)}
 		</ul>
 	);
 }
