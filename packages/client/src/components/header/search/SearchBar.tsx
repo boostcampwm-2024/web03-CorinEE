6c7b2262e740464abad4735948baab5a271d657e
qwer0114 <skyok203@naver.com>
2024-11-28 16:22:57 +0900

    feat: SearchBar component

--- packages/client/src/components/header/search/SearchBar.tsx
+++ packages/client/src/components/header/search/SearchBar.tsx
@@ -6,7 +6,7 @@ function SearchBar() {
 	const { open, handleOpen } = useModal();
 	return (
 		<>
-			<div className="flex items-center  gap-2 bg-blue-gray-50 py-1 px-3 w-48 rounded-xl text-gray-500 text-center">
+			<div className="flex items-center gap-2 bg-blue-gray-50 py-1 px-4 w-48 rounded-xl text-gray-500 text-center">
 				<SearchIcon className="w-4 h-4" />
 				<button onClick={handleOpen}>코인을 검색하세요</button>
 			</div>
