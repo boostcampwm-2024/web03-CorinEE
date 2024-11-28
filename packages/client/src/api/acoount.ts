f7fd33a31d5c733f90d64fc86c5657d615096c7b
qwer0114 <skyok203@naver.com>
2024-11-28 16:21:49 +0900

    feat: account reset api

--- packages/client/src/api/acoount.ts
+++ packages/client/src/api/acoount.ts
@@ -6,3 +6,8 @@ export async function myAccount(): Promise<Account> {
 	const response = await authInstance.get(`/account/myaccount`);
 	return response.data;
 }
+
+export async function resetAccount() {
+	const response = await authInstance.delete('/user/reset');
+	return response.data;
+}
