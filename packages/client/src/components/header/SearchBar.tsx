8b79ce7c25170ca9e1b02869f9d01d18f603c049
qwer0114 <skyok203@naver.com>
2024-11-28 12:33:25 +0900

    feat: 검색바 생성

---
+++ packages/client/src/components/header/SearchBar.tsx
@@ -0,0 +1,18 @@
+import SearchIcon from '@/asset/search.svg?react';
+import SearchModal from '@/components/modal/SearchModal';
+import { useModal } from '@/hooks/ui/useModal';
+
+function SearchBar() {
+	const { open, handleOpen } = useModal();
+	return (
+		<>
+			<div className="flex items-center  gap-2 bg-blue-gray-50 py-1 px-3 w-48 rounded-xl text-gray-500 text-center">
+				<SearchIcon className="w-4 h-4" />
+				<button onClick={handleOpen}>코인을 검색하세요</button>
+			</div>
+			<SearchModal open={open} handleOpen={handleOpen} />
+		</>
+	);
+}
+
+export default SearchBar;
