57a840ae622c79d037a05c39308d6c7ec56242db
qwer0114 <skyok203@naver.com>
2024-11-28 21:58:39 +0900

    refactor: 파라미터 변경

--- packages/client/src/hooks/market/useRecentlyMarket.ts
+++ packages/client/src/hooks/market/useRecentlyMarket.ts
@@ -1,14 +1,17 @@
+import { convertToQueryString } from '@/utility/api/queryString';
 import { useQuery } from '@tanstack/react-query';
 import { getRecentlyMarketList } from '@/api/market';
+import { Interest } from '@/types/interest';
 
-export function useRecentlyMarketList(
-	queryString: string,
-	options?: { enabled?: boolean },
-) {
+export function useRecentlyMarketList(myInterest: Interest[]) {
+	const isEnable = myInterest.length ? true : false;
+	const interestMarketList = myInterest.map((info) => info.assetName);
+	const queryString = convertToQueryString(interestMarketList);
+
 	const { isPending, error, data } = useQuery({
 		queryKey: ['recentlyMarketList', queryString],
 		queryFn: () => getRecentlyMarketList(queryString),
-		enabled: options?.enabled,
+		enabled: isEnable,
 		staleTime: 1000 * 60, // 1분
 		gcTime: 1000 * 60 * 5, // 5분
 	});
