4f1ec8761fe863195887b4c9781adf9f0c38e447
qwer0114 <skyok203@naver.com>
2024-12-01 14:03:40 +0900

    feat: 모든 사용자 계좌 정보 조회 api 추가

--- packages/client/src/api/acoount.ts
+++ packages/client/src/api/acoount.ts
@@ -1,6 +1,6 @@
 import '@/api/interceptors';
-import { authInstance } from '@/api/instance';
-import { Account } from '@/types/account';
+import { authInstance, instance } from '@/api/instance';
+import { Account, UserInvestment } from '@/types/account';
 
 export async function myAccount(): Promise<Account> {
 	const response = await authInstance.get(`/account/myaccount`);
@@ -9,5 +9,10 @@ export async function myAccount(): Promise<Account> {
 
 export async function resetAccount() {
 	const response = await authInstance.delete('/user/reset');
+	return response.data;
+}
+
+export async function getAllUserInvestment(): Promise<UserInvestment[]> {
+	const response = await instance.get('/user/all-users-account');
 	return response.data;
 }
