1a72bec745140c5eeeb77e7768120d46ba6f1dad
qwer0114 <skyok203@naver.com>
2024-12-01 14:04:26 +0900

    feat: ranking 페이지 table header 정의

---
+++ packages/client/src/pages/ranked/RankingTableHeader.tsx
@@ -0,0 +1,16 @@
+function RankingTableHeader() {
+	return (
+		<thead>
+			<tr className="border-b border-solid border-gray-400 text-gray-700">
+				<th className="p-2 text-left w-16">순위</th>
+				<th className="p-2 text-left w-16">사용자</th>
+				<th className="p-2 text-right w-fit">총 자산</th>
+				<th className="p-2 text-right w-fit">투자 비율</th>
+				<th className="p-2 text-right w-fit">총 손익</th>
+				<th className="p-2 text-right w-fit">수익률</th>
+			</tr>
+		</thead>
+	);
+}
+
+export default RankingTableHeader;
