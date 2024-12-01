72ab4eaecee94652210482f670151412d79b99d4
qwer0114 <skyok203@naver.com>
2024-12-01 16:03:38 +0900

    feat: Profile type 정의

--- packages/client/src/api/auth.ts
+++ packages/client/src/api/auth.ts
@@ -1,6 +1,6 @@
 import '@/api/interceptors';
 import { authInstance, instance } from '@/api/instance';
-import { LogOut, Login } from '@/types/auth';
+import { LogOut, Login, Profile } from '@/types/auth';
 
 export async function guestLogin(): Promise<Login> {
 	const response = await instance.post('/auth/guest-login');
@@ -9,5 +9,10 @@ export async function guestLogin(): Promise<Login> {
 
 export async function logout(): Promise<LogOut> {
 	const response = await authInstance.delete('/auth/logout');
+	return response.data;
+}
+
+export async function getProfile(): Promise<Profile> {
+	const response = await authInstance.get('/auth/profile');
 	return response.data;
 }
