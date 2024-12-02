b825d2404b99c326faf458dcece3647ed07e65f6
qwer0114 <skyok203@naver.com>
2024-12-03 00:10:55 +0900

    hotfix: 불필요한 refetch 제거

--- packages/client/src/hooks/chart/usePeriodChart.ts
+++ packages/client/src/hooks/chart/usePeriodChart.ts
@@ -8,33 +8,31 @@ export function usePeriodChart(
 	period: CandlePeriod,
 	minute?: number,
 ) {
-	const { data, fetchNextPage, hasNextPage, refetch } =
-		useSuspenseInfiniteQuery<
-			Candle[],
-			Error,
-			InfiniteCandle,
-			[string, string, CandlePeriod, number?],
-			string | undefined
-		>({
-			queryKey: ['candles', market, period, minute],
-			queryFn: ({ pageParam }) => {
-				return getCandleByPeriod(market, period, pageParam, minute);
-			},
-			getNextPageParam: (lastPage) => {
-				if (lastPage.length === 0) return undefined;
-				const oldestCandle = lastPage[lastPage.length - 1];
-				return oldestCandle?.candle_date_time_utc;
-			},
-			initialPageParam: undefined,
-			select: (data) => ({
-				candles: data.pages.flat(),
-				hasNextPage: data.pages[data.pages.length - 1]?.length === 200,
-			}),
-			refetchInterval: getPeriodMs(period, minute),
-		});
+	const { data, fetchNextPage, hasNextPage } = useSuspenseInfiniteQuery<
+		Candle[],
+		Error,
+		InfiniteCandle,
+		[string, string, CandlePeriod, number?],
+		string | undefined
+	>({
+		queryKey: ['candles', market, period, minute],
+		queryFn: ({ pageParam }) => {
+			return getCandleByPeriod(market, period, pageParam, minute);
+		},
+		getNextPageParam: (lastPage) => {
+			if (lastPage.length === 0) return undefined;
+			const oldestCandle = lastPage[lastPage.length - 1];
+			return oldestCandle?.candle_date_time_utc;
+		},
+		initialPageParam: undefined,
+		select: (data) => ({
+			candles: data.pages.flat(),
+			hasNextPage: data.pages[data.pages.length - 1]?.length === 200,
+		}),
+		refetchInterval: getPeriodMs(period, minute),
+	});
 
 	return {
-		refetch,
 		data,
 		fetchNextPage,
 		hasNextPage,
